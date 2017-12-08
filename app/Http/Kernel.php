a8191c97ea6419ccb8f4c5d1ebaa8940fff52aa8
gutropolis <gutropolis@gmail.com>
2017-12-08 10:17:33 +0530

    changes regarding to admin users

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'admin' => \App\Http\Middleware\SentinelAdmin::class,
+	    'user' => \App\Http\Middleware\SentinelUser::class,
     ];
 }
