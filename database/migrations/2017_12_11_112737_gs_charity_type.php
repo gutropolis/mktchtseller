8c6d48d38176d20f62c00fe7fecbbda4f00acfaf
gutropolis <gutropolis@gmail.com>
2017-12-14 12:21:54 +0530

    charity type add by a

---
+++ database/migrations/2017_12_11_112737_gs_charity_type.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GsCharityType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gs_charity_type', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('parent_id');
+            $table->string('updated_by');
+            $table->timestamps();
+            
+
+        
+        
+		});
+       
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gs_charity_type');
+    }
+}
