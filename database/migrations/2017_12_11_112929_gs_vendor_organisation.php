5438c2905766c3b8c5a438424db5c17175baf378
gutropolis <gutropolis@gmail.com>
2017-12-14 12:30:08 +0530

    add by a

---
+++ database/migrations/2017_12_11_112929_gs_vendor_organisation.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GsVendorOrganisation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gs_vender_organisation', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('location');
+            $table->string('images');
+            $table->string('year_in_business');
+            $table->string('start_up_year');
+            $table->string('misstion_statement');
+            $table->string('vision_statement');
+            $table->string('tax_id');
+            $table->string('business_address');
+            $table->string('phone_number');
+            $table->string('user_id');
+            
+            $table->string('updated_by');
+            
+            $table->timestamps();
+            
+
+        
+        
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gs_vender_organisation');
+    }
+}
