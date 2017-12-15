35818198363dfaea8da3650342110f404ab89cde
gutropolis <gutropolis@gmail.com>
2017-12-15 20:01:56 +0530

    modify

--- app/charity.php
+++ app/charity.php
@@ -3,15 +3,33 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
+use Cviebrock\EloquentTaggable\Taggable;
+//use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class charity extends Model
 {
-    protected $table="gs_charity_organisation";
-   protected $fillable = [
+    protected $table = 'gs_charity_organisation';
+
+    protected $fillable = [
         'title','description','location','images','year_in_business',
 		'start_up_year','business_purpose','address','phone_number',
 		'keyword','vision_statement','mission_statement','tags'
 		];
+    
+        protected $guarded = ['id'];
+    
+        
+        public function category()
+        {
+            return $this->belongsTo(CharityCategory::class,'charity_type');
+        }
+        
+        public function getCharityCategoryAttribute()
+        {
+            return $this->category->pluck('id');
+        }
 }
