2b489a9afe928f5c8ca9cf6e790d90bd4ade4768
gutropolis <gutropolis@gmail.com>
2017-12-15 19:57:05 +0530

    modify by a

--- app/CharityCategory.php
+++ app/CharityCategory.php
@@ -1,20 +1,24 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
-
-class CharityCategory extends Model {
-
-   // use SoftDeletes;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
+class CharityCategory extends model
+{
+    use SoftDeletes;
     //protected $dates = ['deleted_at'];
-
     protected $table = 'gs_charity_type';
-	 protected $fillable = [
-        'title','description','parent_id','updated_by'
-		];
-
+    protected $guarded = ['id'];
+    public function charity()
+    {
+        return $this->hasMany(charity::class);
+    }
+    public function parent()
+    {
+        return $this->belongsTo('CharityCategory', 'parent_id');
+    }
     
+    public function children()
+    {
+        return $this->hasMany('CharityCategory', 'parent_id');
+    }
 }
