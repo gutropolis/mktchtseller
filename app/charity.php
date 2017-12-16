7f6bc34c3e88eede260604b0fee6ebf6925fe360
gutropolis <gutropolis@gmail.com>
2017-12-16 12:40:10 +0530

    modify for charity type

--- app/charity.php
+++ app/charity.php
@@ -17,7 +17,7 @@ class charity extends Model
     protected $fillable = [
         'title','description','location','images','year_in_business',
 		'start_up_year','business_purpose','address','phone_number',
-		'keyword','vision_statement','mission_statement','tags'
+		'keyword','vision_statement','mission_statement','charity_type','tags'
 		];
     
         protected $guarded = ['id'];
