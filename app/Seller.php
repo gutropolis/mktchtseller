12e08be2098885c33f1693770fefcc43fbbd04b3
gutropolis <gutropolis@gmail.com>
2017-12-16 19:44:56 +0530

    add by n

--- app/Seller.php
+++ app/Seller.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Seller extends Model
 {
- protected $table="gs_seller_organisation";
+ protected $table="gs_vender_organisation";
     protected $fillable = [
         'title','description','location','pic','year_in_buisness','start_up_year','address','mission_statement','vision_statement','tax_id','phone_number'
     ];
