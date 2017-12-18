1f059ce006b335fae478dcc1222ca62d46d5574d
gutropolis <gutropolis@gmail.com>
2017-12-18 19:18:20 +0530

    add by n

--- app/Seller.php
+++ app/Seller.php
@@ -8,7 +8,7 @@ class Seller extends Model
 {
  protected $table="gs_vender_organisation";
     protected $fillable = [
-        'title','description','location','pic','year_in_buisness','start_up_year','address','mission_statement','vision_statement','tax_id','phone_number'
+        'title','description','location','pic','year_in_buisness','start_up_year','address','mission_statement','vision_statement','tax_id','phone_number','business_type'
     ];
 
     
