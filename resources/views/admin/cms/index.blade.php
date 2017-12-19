4f312868e0592a3a49bc192f886d9c8559532233
gutropolis <gutropolis@gmail.com>
2017-12-19 18:45:11 +0530

    add by n

--- resources/views/admin/cms/index.blade.php
+++ resources/views/admin/cms/index.blade.php
@@ -1,48 +1,51 @@
 @extends('admin/layouts/default')
 
-{{-- Web site Title --}}
+{{-- Page title --}}
 @section('title')
-@lang('groups/title.management')
+    @lang('CMS ')
 @parent
 @stop
 
-{{-- Content --}}
+{{-- page level styles --}}
+@section('header_styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/datatables/css/dataTables.bootstrap.css') }}" />
+    <link href="{{ asset('assets/css/pages/tables.css') }}" rel="stylesheet" type="text/css" />
+@stop
+
+
+{{-- Page content --}}
 @section('content')
 <section class="content-header">
-    <h1>@lang('CMS Mangement')</h1>
+    <h1>@lang('CMS Content')</h1>
     <ol class="breadcrumb">
         <li>
-            <a href="{{ route('admin.dashboard') }}">
-                <i class="livicon" data-name="cms" data-size="14" data-color="#000"></i>
+            <a href="{{ route('admin.dashboard') }}"> <i class="livicon" data-name="home" data-size="16" data-color="#000"></i>
                 @lang('general.dashboard')
             </a>
         </li>
-        <li><a href="#"> @lang('groups/title.cms')</a></li>
-        <li class="active">@lang('groups/title.cms_list')</li>
+        <li><a href="#">@lang('CMS')</a></li>
+        <li class="active">@lang('CMS Content')</li>
     </ol>
 </section>
 
 <!-- Main content -->
-<section class="content">
+<section class="content paddingleft_right15">
     <div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-primary ">
-                <div class="panel-heading clearfix">
-                    <h4 class="panel-title pull-left"> <i class="livicon" data-name="cms" data-size="16" data-loop="true" data-c="#fff" data-hc="white"></i>
-                        @lang('CMS')
-                    </h4>
-                    <div class="pull-right">
+        <div class="panel panel-primary ">
+            <div class="panel-heading clearfix">
+                <h4 class="panel-title pull-left"> <i class="livicon" data-name="users" data-size="16" data-loop="true" data-c="#fff" data-hc="white"></i>
+                    @lang('CMS Content')
+                </h4>
+                <div class="pull-right">
                     <a href="{{ route('admin.cms.create') }}" class="btn btn-sm btn-default"><span class="glyphicon glyphicon-plus"></span> @lang('cms.create')</a>
-                    </div>
                 </div>
-                <br />
-                <div class="panel-body">
-                    
-                        <div class="table-responsive">
-
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr>
+            </div>
+            <br />
+            <div class="panel-body">
+                <div class="table-responsive">
+                <table class="table table-bordered" id="table">
+                    <thead>
+                       <tr>
                                     <th>@lang('ID')</th>
                                     
                                     <th>@lang('Title')</th>
@@ -52,12 +55,11 @@
 									<th>@lang('created_at')</th>
 									<th>@lang('Actions')</th>
                                 </tr>
-                            </thead>
-                            <tbody>
-
-                                @foreach ($cms as $role)
-								
-                                <tr>
+                    </thead>
+                    <tbody>
+                    
+                            @foreach ($cms as $role)
+                             <tr>
                                     <td>{!! $role->id !!}</td>
                                    
 									<td>{!! $role->title!!}</td>
@@ -83,49 +85,35 @@
                                 </td>
                                 </tr>
                                 @endforeach
-                            </tbody>
-                        </table>
-                        </div>
-                 
-                        @lang('general.noresults')
-                    
+                </table>
                 </div>
             </div>
         </div>
     </div>    <!-- row-->
 </section>
-
-
-
-
 @stop
 
-{{-- Body Bottom confirm modal --}}
+{{-- page level scripts --}}
 @section('footer_scripts')
+    <script type="text/javascript" src="{{ asset('assets/vendors/datatables/js/jquery.dataTables.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/vendors/datatables/js/dataTables.bootstrap.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#table').DataTable();
+        });
+    </script>
+
 <div class="modal fade" id="delete_confirm" tabindex="-1" role="dialog" aria-labelledby="user_delete_confirm_title" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-    </div>
+	<div class="modal-dialog">
+    	<div class="modal-content"></div>
   </div>
 </div>
-<div class="modal fade" id="users_exists" tabindex="-2" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-            </div>
-            <div class="modal-body">
-                @lang('groups/message.users_exists')
-            </div>
-        </div>
-    </div>
-</div>
 <script>
-    $(function () {$('body').on('hidden.bs.modal', '.modal', function () {$(this).removeData('bs.modal');});});
-    $(document).on("click", ".users_exists", function () {
-
-        var group_name = $(this).data('name');
-        $(".modal-header h4").text( group_name+" Group" );
-    });</script>
+$(function () {
+	$('body').on('hidden.bs.modal', '.modal', function () {
+		$(this).removeData('bs.modal');
+	});
+});
+</script>
 @stop
