34215b6c941649cf3edeca19640a7aae1dd2acf9
gutropolis <gutropolis@gmail.com>
2017-12-19 15:31:07 +0530

    add by n

---
+++ app/Sellerproduct.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sellerproduct extends Model
+{
+ protected $table="gs_vender_product";
+    protected $fillable = [
+        'title','description','asin_url','images','reviews','user_id'
+    ];
+
+    
+}
