8039460f9ef65522faec0659e3f4ed7b584b6eb8
gutropolis <gutropolis@gmail.com>
2018-01-03 10:35:28 +0530

    add by n

---
+++ resources/assets/js/routes.js
@@ -0,0 +1,78 @@
+import VueRouter from 'vue-router'
+import helper from './services/helper' 
+
+let routes = [
+    {
+        path: '/',
+        component: require('./layouts/default-page'),
+        meta: { requiresGuest: true },
+        children: [
+			{
+                path: '/',
+                component: require('./views/pages/home')
+            },
+			{
+                path: '/login',
+                component: require('./views/auth/login')
+            },
+			{
+                path: '/register',
+                component: require('./views/auth/register')
+            },
+			{
+                path: '/contact',
+                component: require('./views/pages/contact')
+            },
+			{
+				path: '/seller',
+				component:require('./views/pages/seller')
+			},
+			{
+				path: '/product',
+				component:require('./views/pages/seller_product')
+			},
+			{
+				path: '/sellerfab',
+				component:require('./views/pages/sellersearch')
+			},
+			{
+				path : '/my_account',
+				component:require('./views/pages/my_account')
+			},
+			{
+				path : 'my_ads',
+				component:require('./views/pages/my_ads')
+			},
+			{
+				path : 'my_notification',
+				component:require('./views/pages/my_notification')
+			},
+			{
+				path : 'my_message',
+				component:require('./views/pages/my_message')
+			},
+             
+        ]
+    },
+	 
+    {
+        path: '*',
+        component : require('./layouts/error-page'),
+        children: [
+            {
+                path: '*',
+                component: require('./views/errors/page-not-found')
+            }
+        ]
+    }
+];
+
+const router = new VueRouter({
+	routes,
+    linkActiveClass: 'active',
+    mode: 'history'
+});
+
+ 
+
+export default router;
