436b45002633baa4850a061894454e43cb4ef6aa
gutropolis <gutropolis@gmail.com>
2018-01-03 10:16:05 +0530

    add by n

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,27 +1,15 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+ 
 
-window._ = require('lodash');
-
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
-} catch (e) {}
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
+window.Vue = Vue;
+Vue.use(VueRouter);
+window.axios = axios;
+//window.Form = Form;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('auth_token');
 
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
