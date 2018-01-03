4fe15bdf30e8bfa3909bf82c22f6043331c19c97
gutropolis <gutropolis@gmail.com>
2018-01-03 10:35:01 +0530

    add by n

---
+++ resources/assets/js/pages/home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div class="row page-titles">
+            <div class="col-md-6 col-8 align-self-center">
+                <h3 class="text-themecolor m-b-0 m-t-0">Home</h3>
+            </div>
+        </div>
+
+        
+</template>
+
