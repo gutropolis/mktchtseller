5a7ab8a32324cfaeb7010274f9cb292ef5e96c64
gutropolis <gutropolis@gmail.com>
2018-01-03 10:42:12 +0530

    add by n

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+     <title>Charity</title>
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,800">
+    <link rel='stylesheet' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+  <link href="css/toastr.min.css" rel="stylesheet" type="text/css">  
+ </head>
+ <body >
+        
+     <div id="root">
+         <router-view></router-view>
+     </div>
+     <script src="/js/bundle.min.js"></script>
+   <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+ <script src="js/toastr.min.js"></script>
+ </body>
+</html>
