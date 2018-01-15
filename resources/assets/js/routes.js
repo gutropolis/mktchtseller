ad9d4ecf26a1fa0aafbe55b8a2ed4e5c930a2840
gutropolis <gutropolis@gmail.com>
2018-01-15 10:30:53 +0530

    add by n

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -16,6 +16,14 @@ let routes = [
                 component: require('./views/auth/login')
             },
 			{
+                path: '/forget_password',
+                component: require('./views/auth/forget_password')
+            },
+			{
+				path : 'password',
+				component:require('./views/auth/change_password')
+			},
+			{
                 path: '/register',
                 component: require('./views/auth/register')
             },
@@ -28,12 +36,29 @@ let routes = [
                 component: require('./views/pages/aboutus')
 			},
 			{
-				path: '/seller',
-				component:require('./views/pages/seller')
+				path: '/vender_organisation',
+				component:require('./views/pages/manage seller/vender_organisation')
 			},
 			{
+				path: '/product_list',
+				component:require('./views/pages/manage seller/product_list')
+			},
+			
+			{
+				path: '/vender_Category',
+				component:require('./views/pages/manage seller/vender_category')
+			},
+			
+			
+			{ 
+				name : 'edit_product',
+                path: '/edit',
+                component: require('./views/pages/manage seller/edit')
+            },
+			
+			{
 				path: '/product',
-				component:require('./views/pages/seller_product')
+				component:require('./views/pages/manage seller/seller_product')
 			},
 			{
 				path: '/charityfba',
@@ -41,17 +66,31 @@ let routes = [
 			},
 			{
 				path: '/sellerfab',
-				component:require('./views/pages/sellersearch')
+				component:require('./views/pages/manage seller/sellersearch')
 			},
 			{
 				path : '/my_account',
-				component:require('./views/pages/my_account')
+				component:require('./views/pages/users/my_account')
 			},
 			{
 				path : 'my_ads',
 				component:require('./views/pages/my_ads')
 			},
+				{ 
+				name : 'edit_ads',
+                path: '/edit',
+                component: require('./views/pages/my_ads/edit_ads')
+            },
 			{
+				path : 'create_ads',
+				component:require('./views/pages/my_ads/create_ads')
+			},
+			{
+				path : 'show_ads',
+				component:require('./views/pages/my_ads/show_ads')
+			},
+			{
+				
 				path : 'my_notification',
 				component:require('./views/pages/my_notification')
 			},
@@ -59,6 +98,11 @@ let routes = [
 				path : 'my_message',
 				component:require('./views/pages/my_message')
 			},
+			{
+				path : 'my_profile',
+				component:require('./views/pages/users/edit_profile')
+			},
+			
              
         ]
     },
@@ -75,12 +119,12 @@ let routes = [
     }
 ];
 
+
 const router = new VueRouter({
 	routes,
     linkActiveClass: 'active',
     mode: 'history'
 });
 
- 
 
 export default router;
