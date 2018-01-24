8795f14bdd1289bedcbfe2b88e0fba97431d284e
gutropolis <gutropolis@gmail.com>
2018-01-24 15:42:59 +0530

    add toaster file

--- webpack.mix.js
+++ webpack.mix.js
@@ -22,7 +22,8 @@ mix.js('resources/assets/js/app.js', 'public/js/app.js')
 		plugin + 'bootstrap.min.js',
 		plugin + 'paccordion.js'  ,
 		plugin + 'custom.js'  ,
-		plugin + 'tether.min.js'  ,
+		plugin + 'tether.min.js',
+		plugin + 'toastr.min.js',
 		'public/js/app.js'
 ],'public/js/bundle.min.js') 
 .browserSync();
