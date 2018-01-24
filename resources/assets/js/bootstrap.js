3d0e70654f3ac1e0e1d21db3619cebb5d57a2097
gutropolis <gutropolis@gmail.com>
2018-01-24 11:22:37 +0530

    bootstrap

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import axios from 'axios'
- 
-
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
 window.Vue = Vue;
 Vue.use(VueRouter);
 window.axios = axios;
