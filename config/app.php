5f3f6349cc51434bd5362000f49b79125e0f8d05
gutropolis <gutropolis@gmail.com>
2018-01-25 15:10:10 +0530

    socail

--- config/app.php
+++ config/app.php
@@ -179,7 +179,7 @@ return [
 		Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
 		Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 		Intervention\Image\ImageServiceProvider::class,
-		
+		 Laravel\Socialite\SocialiteServiceProvider::class,
 
 
 		
@@ -230,6 +230,8 @@ return [
         'Route' => Illuminate\Support\Facades\Route::class,
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
+		'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
