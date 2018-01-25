7a09973638c7eceb5d1d9dc23910d467b746f1ee
gutropolis <gutropolis@gmail.com>
2018-01-25 10:20:59 +0530

    modified css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,10 @@
 	<link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,800">
     <link rel='stylesheet' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	 <link rel="stylesheet" href="css/vue-form-wizard.min.css">
+	 <link href="css/toastr.min.css" rel="stylesheet" type="text/css">
+	  <link rel="stylesheet" href="css/vue-form-wizard.min.css">
+  <link href="css/toastr.min.css" rel="stylesheet" type="text/css">
    
  
  </head>
