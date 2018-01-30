d5727e00d1566086c78a18ca626627fd5b90d9fe
gutropolis <gutropolis@gmail.com>
2018-01-30 18:04:23 +0530

    modified for imag

---
+++ resources/assets/js/views/pages/users/image_upload.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+    <div id="main-wrapper">
+<app-navbar></app-navbar>
+	    <section class=" my__account">
+        <div class="row">
+            <app-sidebar></app-sidebar>
+       
+    
+                <div class="col-md-9 dashboard">
+    
+    
+    
+                    <div class="dashboard__content clearfix">
+    
+    
+    
+                        <div class="dashboard__content--head">
+						 <h3 class="dashboard__content--head--heading">Change Profile Pic</h3>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-md-2">
+                <img :src="image" class="img-responsive">
+            </div>
+            <div class="col-md-8">
+                <input type="file" v-on:change="onFileChange" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-success btn-block" @click="upload">Upload</button>
+            </div>
+        </div>
+    </div>
+	</div>
+	
+                    </div>
+    
+    </div>
+                </div>
+    
+	</section>
+	</div>
+	</div>
+	
+</template>
+<script>
+ import AppNavbar from './navbar.vue' 
+ import AppSidebar from './sidebar.vue'
+ import Vue from 'vue'
+    export default {
+    components: {
+            AppNavbar,  AppSidebar 
+        },
+ data() {
+            return {
+				 image: ''
+			};
+			},
+  methods: {
+ onFileChange(e) {
+                let files = e.target.files || e.dataTransfer.files;
+                if (!files.length)
+                    return;
+                this.createImage(files[0]);
+            },
+            createImage(file) {
+                let reader = new FileReader();
+                let vm = this;
+                reader.onload = (e) => {
+                    vm.image = e.target.result;
+                };
+                reader.readAsDataURL(file);
+            },
+            upload(){
+                axios.post('/api/upload',{image: this.image}).then(response => {
+					 toastr['success'](response.data.message);
+                });
+				},
+		}
+} 	
+</script>
