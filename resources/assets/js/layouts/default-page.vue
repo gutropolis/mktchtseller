4769b9d3880841f90c52000b4026009354ef1ad6
gutropolis <gutropolis@gmail.com>
2018-01-31 12:08:39 +0530

    profile pic

--- resources/assets/js/layouts/default-page.vue
+++ resources/assets/js/layouts/default-page.vue
@@ -11,9 +11,37 @@
  import AppHeader from './header.vue' 
 
  import AppFooter from './footer.vue'
+  import helper from '../services/helper'
     export default {
+	 methods : {
+            notification(){
+                toastr.options = {
+                    "positionClass": "toast-top-right"
+                };
+
+                $('[data-toastr]').on('click',function(){
+                    var type = $(this).data('toastr'),message = $(this).data('message'),title = $(this).data('title');
+                    toastr[type](message, title);
+                });
+            },
+            getAuthUser(name){
+                return this.$store.getters.getAuthUser(name);
+            }
+        },
         components: {
             AppHeader,AppFooter 
-        } 
+        }, 
+		 mounted() {
+		if(!this.getAuthUser('email')){
+                helper.authUser().then(response => {
+                    this.$store.dispatch('setAuthUserDetail',{
+                        first_name: response.user.first_name,
+                        last_name: response.user.last_name,
+                        email: response.user.email,
+                        avatar:response.user.avatar
+                    });
+                });
+            }
     }
+	}
 </script>
