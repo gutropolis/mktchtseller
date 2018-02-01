beea62cf665eb3a5a297f5962de52ed034c68700
gutropolis <gutropolis@gmail.com>
2018-02-01 17:21:29 +0530

    changes sidebar

--- resources/assets/js/views/pages/manage_seller/seller_list.vue
+++ resources/assets/js/views/pages/manage_seller/seller_list.vue
@@ -54,8 +54,8 @@
 
 <script>
 
-import AppNavbar from './navbar.vue' 
- import AppSidebar from './sidebar.vue'
+import AppNavbar from '../users/navbar.vue' 
+ import AppSidebar from '../users/sidebar.vue'
     export default {
         components: {
             AppNavbar,  AppSidebar 
