74caa62fba1354a86bcc8e3a702dde3478d26289
gutropolis <gutropolis@gmail.com>
2018-02-01 17:18:00 +0530

    siderbar add

--- resources/assets/js/views/pages/manage_seller/edit_seller.vue
+++ resources/assets/js/views/pages/manage_seller/edit_seller.vue
@@ -89,8 +89,8 @@
 </div>
 </template>
 <script>
-import AppNavbar from './navbar.vue' 
- import AppSidebar from './sidebar.vue'
+import AppNavbar from '../users/navbar.vue' 
+ import AppSidebar from '../users/sidebar.vue'
 import Vue from 'vue'
  import VueFormWizard from 'vue-form-wizard'
  Vue.use(VueFormWizard)
