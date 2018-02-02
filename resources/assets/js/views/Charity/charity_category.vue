17724ce20dd402f45e2031ea036ecb3c94acdebe
gutropolis <gutropolis@gmail.com>
2018-02-02 16:07:02 +0530

    sidebar

--- resources/assets/js/views/Charity/charity_category.vue
+++ resources/assets/js/views/Charity/charity_category.vue
@@ -1,167 +1,12 @@
 <template>
-<div>
- <section class="page__head">
-        <div class="container">
-            <div class="page__head--content">
-                <h2 class="page__head--content--heading">My Account</h2>
-                <a href="#" class="page__head--content--menu">Home </a> >
-                <a href="#" class="page__head--content--menu">My Account</a>
-
-            </div>
-        </div>
-    </section>
+<div id="main-wrapper">
+<app-navbar></app-navbar>
 	    <section class=" my__account">
         <div class="row">
-            <aside class="col-md-3 proflie__element">
-                <figure class="proflie__element--image">
-                    <img src="images/profile-dashboard.jpg">
-                </figure>
-				
-                <h4 class="proflie__element--name">John Smith</h4>
-
-                <div class="proflie__element--detail">
-                    <ul class="proflie__element--detail--list">
-                        <li class="proflie__element--detail--list--item">
-                            <router-link to="/my_account"  class="proflie__element--detail--list--item--link">
-                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 486.9 486.9" style="enable-background:new 0 0 486.9 486.9;" xml:space="preserve">
-                                       <g>
-                                           <path d="M276.8,364.45c0-19.8,4.9-38.9,14.2-55.9c-13.2-7.4-22.9-12.3-26.9-14.2c-1.2-0.6-2-1.9-2-3.2v-78
-                                               c9.8-6.5,16.3-17.7,16.3-30.4v-80.9c0-40.3-32.6-72.9-72.9-72.9h-8.7h-8.7c-40.3,0-72.9,32.6-72.9,72.9v80.9
-                                               c0,12.7,6.5,23.8,16.3,30.4v78c0,1.4-0.8,2.6-2,3.2c-11,5.4-65,32.7-115.2,73.9c-9.1,7.4-14.3,18.6-14.3,30.3v55.4h196.7h121.8
-                                               c-2.6-2.2-5.2-4.5-7.6-7C288.9,424.95,276.8,395.55,276.8,364.45z"/>
-                                       </g>
-                                       <g>
-                                           <path d="M486.9,358.25c0-4.4-3.5-7.9-7.9-7.9h-12c-1.9-9.9-5.8-19.4-11.7-28l8.5-8.5c3.1-3.1,3.1-8.1,0-11.2l-8.6-8.6
-                                               c-3.1-3.1-8.1-3.1-11.2,0l-8.5,8.5c-8.6-5.9-18.2-9.7-28.1-11.6v-12c0-4.4-3.5-7.9-7.9-7.9h-12.2c-4.4,0-7.9,3.5-7.9,7.9v12
-                                               c-9.9,1.9-19.5,5.7-28.2,11.6l-8.4-8.4c-3.1-3.1-8.1-3.1-11.2,0l-8.6,8.6c-3.1,3.1-3.1,8.1,0,11.2l8.3,8.3
-                                               c-6,8.7-9.9,18.3-11.8,28.3h-11.7c-4.4,0-7.9,3.5-7.9,7.9v12.2c0,4.4,3.5,7.9,7.9,7.9h11.6c1.9,10,5.8,19.7,11.8,28.4l-8.2,8.2
-                                               c-3.1,3.1-3.1,8.1,0,11.2l8.6,8.6c3.1,3.1,8.1,3.1,11.2,0l8.2-8.2c8.7,5.9,18.4,9.8,28.4,11.7v11.6c0,4.4,3.5,7.9,7.9,7.9h12.2
-                                               c4.4,0,7.9-3.5,7.9-7.9v-11.7c10-1.9,19.6-5.9,28.3-11.8l8.3,8.3c3.1,3.1,8.1,3.1,11.2,0l8.6-8.6c3.1-3.1,3.1-8.1,0-11.2
-                                               l-8.4-8.4c5.9-8.7,9.7-18.3,11.6-28.2h12c4.4,0,7.9-3.5,7.9-7.9L486.9,358.25z M422.3,393.15c-15.9,15.9-41.6,15.9-57.5,0.1
-                                               c-15.9-15.9-15.9-41.6-0.1-57.5c15.9-15.9,41.6-15.9,57.5-0.1C438.2,351.45,438.2,377.25,422.3,393.15z"/>
-                                       </g>
-                               </svg> My Profile</router-link>
-                        </li>
-                        <li class="proflie__element--detail--list--item">
-                            
-
-						   <router-link to="/my_ads" class="proflie__element--detail--list--item--link">
-                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="31.979px" height="31.979px" viewBox="0 0 31.979 31.979" style="enable-background:new 0 0 31.979 31.979;" xml:space="preserve">
-                                    <g>
-                                       <path d="M21.945,6.06V5.359c0-0.796-0.646-1.441-1.441-1.441h-2.365c0.197-0.356,0.309-0.797,0.309-1.245
-                                           C18.447,1.2,17.249,0,15.777,0c-1.472,0-2.669,1.2-2.669,2.673c0,0.448,0.112,0.889,0.309,1.245h-1.94
-                                           c-0.796,0-1.441,0.645-1.441,1.441V6.06H6.822c-1.207,0-2.185,0.979-2.185,2.186v21.546c0,1.207,0.978,2.187,2.185,2.187h18.334
-                                           c1.207,0,2.186-0.979,2.186-2.187V8.246c0-1.207-0.979-2.186-2.186-2.186H21.945z M14.187,2.674c0-0.878,0.713-1.594,1.589-1.594
-                                           c0.878,0,1.591,0.716,1.591,1.594c0,0.502-0.234,0.933-0.6,1.245h-1.981C14.421,3.606,14.187,3.176,14.187,2.674z M25.156,28.492
-                                           c0,0.742-0.602,1.344-1.344,1.344H8.166c-0.742,0-1.344-0.602-1.344-1.344V9.59c0-0.743,0.602-1.344,1.344-1.344h1.868
-                                           c0,0.591,0.479,1.071,1.07,1.071h9.769c0.592,0,1.07-0.479,1.07-1.071h1.869c0.741,0,1.344,0.602,1.344,1.344V28.492
-                                           L25.156,28.492z"/>
-                                       <circle cx="10.852" cy="13.098" r="1.899"/>
-                                       <path d="M21.656,12.038H15.18c-0.596,0-1.08,0.475-1.08,1.071c0,0.596,0.483,1.071,1.08,1.071h6.478
-                                           c0.596,0,1.078-0.475,1.078-1.071C22.736,12.513,22.253,12.038,21.656,12.038z"/>
-                                       <circle cx="10.852" cy="18.485" r="1.899"/>
-                                       <path d="M21.656,17.391H15.18c-0.596,0-1.08,0.498-1.08,1.094s0.483,1.092,1.08,1.092h6.478c0.596,0,1.078-0.496,1.078-1.092
-                                           S22.253,17.391,21.656,17.391z"/>
-                                       <circle cx="10.852" cy="24.242" r="1.898"/>
-                                       <path d="M21.656,23.145H15.18c-0.596,0-1.08,0.498-1.08,1.094s0.483,1.092,1.08,1.092h6.478c0.596,0,1.078-0.497,1.078-1.092
-                                           C22.736,23.643,22.253,23.145,21.656,23.145z"/>
-                                   </g>
-                               </svg> My ad's</router-link>
-                        </li>
-                        <li class="proflie__element--detail--list--item">
-                          <router-link to="/my_notification"  class="proflie__element--detail--list--item--link">
-                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="535.5px" height="535.5px" viewBox="0 0 535.5 535.5" style="enable-background:new 0 0 535.5 535.5;" xml:space="preserve">
-                                   <g id="notifications-on">
-                                       <path d="M142.8,53.55l-35.7-35.7C45.9,63.75,5.1,135.15,0,216.75h51C56.1,147.9,89.25,89.25,142.8,53.55z M484.5,216.75h51
-                                           c-5.1-81.6-43.35-153-104.55-198.9l-35.7,35.7C446.25,89.25,479.4,147.9,484.5,216.75z M433.5,229.5
-                                           c0-79.05-53.55-142.8-127.5-160.65V51c0-20.4-17.85-38.25-38.25-38.25c-20.4,0-38.25,17.85-38.25,38.25v17.85
-                                           C155.55,86.7,102,150.45,102,229.5v140.25l-51,51v25.5h433.5v-25.5l-51-51V229.5z M267.75,522.75c2.55,0,7.65,0,10.2,0
-                                           c17.85-2.55,30.6-15.3,35.7-30.6c2.55-5.101,5.1-12.75,5.1-20.4h-102C216.75,499.8,239.7,522.75,267.75,522.75z"/>
-                                   </g>
-                               </svg> Notifications</router-link>
-                        </li>
-                        <li class="proflie__element--detail--list--item">
-                         <router-link to="/my_message" class="proflie__element--detail--list--item--link">
-                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 490 490" style="enable-background:new 0 0 490 490;" xml:space="preserve">
-                                   <g>
-                                       <polygon points="169.6,256.3 0,101.5 0,390.4 		"/>
-                                       <path d="M304.5,270.3l-52.8,48.2c-4.1,3.8-10.5,3.8-14.6,0l-52-48.2l-165.6,131h451L304.5,270.3z"/>
-                                       <polygon points="244.6,296.7 472.8,88.7 16.4,88.7 		"/>
-                                       <polygon points="320,256.3 490,390.4 490,101.1 		"/>
-                                   </g>
-                               
-                               </svg> Messages</router-link>
-                        </li
-						  <li class="proflie__element--detail--list--item">
-                         <router-link to="/charity_organisation" class="proflie__element--detail--list--item--link">
-                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 490 490" style="enable-background:new 0 0 490 490;" xml:space="preserve">
-                                   <g>
-                                       <polygon points="169.6,256.3 0,101.5 0,390.4 		"/>
-                                       <path d="M304.5,270.3l-52.8,48.2c-4.1,3.8-10.5,3.8-14.6,0l-52-48.2l-165.6,131h451L304.5,270.3z"/>
-                                       <polygon points="244.6,296.7 472.8,88.7 16.4,88.7 		"/>
-                                       <polygon points="320,256.3 490,390.4 490,101.1 		"/>
-                                   </g>
-                               
-                               </svg> charity_organisation</router-link>
-                        </li>
-						
-						 <li class="proflie__element--detail--list--item">
-                         <router-link to="/charity_category" class="proflie__element--detail--list--item--link">
-                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 490 490" style="enable-background:new 0 0 490 490;" xml:space="preserve">
-                                   <g>
-                                       <polygon points="169.6,256.3 0,101.5 0,390.4 		"/>
-                                       <path d="M304.5,270.3l-52.8,48.2c-4.1,3.8-10.5,3.8-14.6,0l-52-48.2l-165.6,131h451L304.5,270.3z"/>
-                                       <polygon points="244.6,296.7 472.8,88.7 16.4,88.7 		"/>
-                                       <polygon points="320,256.3 490,390.4 490,101.1 		"/>
-                                   </g>
-                               
-                               </svg> charity_category</router-link>
-                        </li>
-						
-						
-                        <li class="proflie__element--detail--list--item">
-                            <a href="#" class="proflie__element--detail--list--item--link">
-                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="44.816px" height="44.816px" viewBox="0 0 44.816 44.816" style="enable-background:new 0 0 44.816 44.816;" xml:space="preserve">
-                                   <g>
-                                       <path d="M22.404,21.173c2.126,0,3.895-1.724,3.895-3.85V3.849C26.299,1.724,24.53,0,22.404,0c-2.126,0-3.895,1.724-3.895,3.849
-                                           v13.475C18.51,19.449,20.278,21.173,22.404,21.173z"/>
-                                       <path d="M30.727,3.33c-0.481-0.2-1.03-0.147-1.466,0.142c-0.434,0.289-0.695,0.776-0.695,1.298v5.113
-                                           c0,0.56,0.301,1.076,0.784,1.354c4.192,2.407,6.918,6.884,6.918,11.999c0,7.654-6.217,13.882-13.87,13.882
-                                           c-7.654,0-13.86-6.228-13.86-13.882c0-5.113,2.813-9.589,6.931-11.997c0.478-0.279,0.773-0.794,0.773-1.348V4.769
-                                           c0-0.521-0.261-1.009-0.695-1.298c-0.435-0.29-0.984-0.342-1.466-0.142C6.257,6.593,0.845,14.276,0.845,23.236
-                                           c0,11.92,9.653,21.58,21.572,21.58c11.917,0,21.555-9.66,21.555-21.58C43.971,14.276,38.554,6.593,30.727,3.33z"/>
-                                   </g>
-                               
-                               </svg> Log Out</a>
-                        </li>
-                    </ul>
-                    <div class="contact__btn">
-                        <a href="#" class="btn contact__btn--content">Contact</a>
-                    </div>
-                </div>
-			</tr>
-            </aside>
+            <app-sidebar></app-sidebar>
             <div class="col-md-9 dashboard">
-                <div class="dashboard__content clearfix">
-                    <div class="dashboard__content--head">
-                        
-                        <a href="#" class="dashboard__content--head--link"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                            width="460.298px" height="460.297px" viewBox="0 0 460.298 460.297" style="enable-background:new 0 0 460.298 460.297;"
-                            xml:space="preserve">
-                           <g>
-                               <path d="M230.149,120.939L65.986,256.274c0,0.191-0.048,0.472-0.144,0.855c-0.094,0.38-0.144,0.656-0.144,0.852v137.041
-                                   c0,4.948,1.809,9.236,5.426,12.847c3.616,3.613,7.898,5.431,12.847,5.431h109.63V303.664h73.097v109.64h109.629
-                                   c4.948,0,9.236-1.814,12.847-5.435c3.617-3.607,5.432-7.898,5.432-12.847V257.981c0-0.76-0.104-1.334-0.288-1.707L230.149,120.939
-                                   z"/>
-                               <path d="M457.122,225.438L394.6,173.476V56.989c0-2.663-0.856-4.853-2.574-6.567c-1.704-1.712-3.894-2.568-6.563-2.568h-54.816
-                                   c-2.666,0-4.855,0.856-6.57,2.568c-1.711,1.714-2.566,3.905-2.566,6.567v55.673l-69.662-58.245
-                                   c-6.084-4.949-13.318-7.423-21.694-7.423c-8.375,0-15.608,2.474-21.698,7.423L3.172,225.438c-1.903,1.52-2.946,3.566-3.14,6.136
-                                   c-0.193,2.568,0.472,4.811,1.997,6.713l17.701,21.128c1.525,1.712,3.521,2.759,5.996,3.142c2.285,0.192,4.57-0.476,6.855-1.998
-                                   L230.149,95.817l197.57,164.741c1.526,1.328,3.521,1.991,5.996,1.991h0.858c2.471-0.376,4.463-1.43,5.996-3.138l17.703-21.125
-                                   c1.522-1.906,2.189-4.145,1.991-6.716C460.068,229.007,459.021,226.961,457.122,225.438z"/>
-                           </g>
-                       </svg>Home</a>
-                    </div>
+                <div class="dashboard__content clearfix">   
+                    
                     <div class="dashboard__content--outer">
                         
                          <form id="savecategory" @submit.prevent="submit">
@@ -201,13 +46,17 @@
 </div>
 </template>
 <script>
+import AppNavbar from '../pages/users/navbar.vue' 
+ import AppSidebar from '../pages/users/sidebar.vue'
 import Vue from 'vue';
 import VeeValidate from 'vee-validate';
  
 Vue.use(VeeValidate);
 
  export default {
-	
+	 components: {
+            AppNavbar,  AppSidebar 
+        },
 	        data() {
 			
             return {
