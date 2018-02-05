fd6e5b34fe9e2c3604813eaeca6f8a352aeb2376
gutropolis <gutropolis@gmail.com>
2018-02-05 15:41:05 +0530

    fb

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -20,7 +20,7 @@ class SocialAuthController extends Controller
         try {
             $user = Socialite::driver($provider)->user();
         } catch (Exception $e) {
-            return redirect('/auth/social');
+            return redirect('/login');
         }
 
         $user_exists = \App\User::whereEmail($user->email)->first();
@@ -40,7 +40,7 @@ class SocialAuthController extends Controller
         }
 
         \Cache::put('jwt_token', $token, 1);
-        return redirect('/auth/social');
+        return redirect('/login');
     }
 
     public function getToken(){
