e31df0b76896a7bae405a8cb2eb4a15caf81e858
gutropolis <gutropolis@gmail.com>
2018-02-08 15:51:43 +0530

    hfh

--- resources/assets/js/views/auth/social-auth.vue
+++ resources/assets/js/views/auth/social-auth.vue
@@ -3,14 +3,14 @@
        
             <div class="login-box card">
             <div class="card-body">
-                <h3 class="box-title m-b-20 text-center">Logging in...</h3>
+                <h3 class="box-title m-b-20 text-center">Loging in...</h3>
                 <div class="form-group m-b-0">
                     <div class="col-sm-12 text-center">
                     <p>Back to Login? <router-link to="/login" class="text-info m-l-5"><b>Sign In</b></router-link></p>
                     </div>
                 </div>
             </div>
-           
+            <guest-footer></guest-footer>
           </div>
         </div>
 
@@ -18,12 +18,12 @@
 </template>
 
 <script>
-    
+    import GuestFooter from '../../layouts/guest-footer.vue'
     export default {
         data() {
             return {}
         },
-        
+       
         mounted(){
             axios.post('/api/auth/social/token').then(response => {
                 localStorage.setItem('auth_token',response.data.token);
