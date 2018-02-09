1514520a31bd2295cb8e50479bef55a81f877b7e
gutropolis <gutropolis@gmail.com>
2018-02-09 10:58:11 +0530

    auth controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -64,6 +64,18 @@ public function index(){
 
         return response()->json(['message' => 'Avatar updated!','profile' => $user]);
     }
+	
+	public function role(Request $request)
+	{
+		 $user = JWTAuth::parseToken()->authenticate();
+		$user->role = request('role');
+		$user->save();
+		return response()->json(['message' => 'Role Selected']);
+	}
+	
+	
+	
+	
 
     public function removeAvatar(Request $request){
 
