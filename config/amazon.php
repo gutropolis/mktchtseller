f714fa0d2da6a62bfebfda4dbfef8bd75311ad65
gutropolis <gutropolis@gmail.com>
2018-02-14 13:01:00 +0530

    product api

---
+++ config/amazon.php
@@ -0,0 +1,81 @@
+<?php
+
+return [
+    
+	// Amazon access key
+	'access_key' => env('AMAZON_ACCESS_KEY'),
+	
+	// Amazon secret key
+	'secret_key' => env('AMAZON_SECRET_KEY'),
+	
+	// Associate tag
+	'associate_tag' => env('AMAZON_ASSOCIATE_TAG'),
+	
+	// Locale
+	// Options: 'co.uk', 'com', 'ca', 'com.br', 'de', 'es', 'fr', 'in', 'it', 'co.jp', 'com.mx'
+	'locale' => 'in',
+	
+	// Available Locales
+	'available_locales' => [
+		'co.uk',
+		'com',
+		'ca',
+		'com.br',
+		'de',
+		'es',
+		'fr',
+		'in',
+		'it',
+		'co.jp',
+		'com.mx'
+		],
+			
+	// Default response group
+	'response_group' => env('AMAZON_RESPONSE_GROUP', 'Images,ItemAttributes,OfferSummary'),
+	
+	// Amazon categories
+	'categories' => [
+		'All',
+		'Apparel',
+		'Appliances',
+		'Automotive',
+		'Baby',
+		'Beauty',
+		'Blended',
+		'Books',
+		'Classical',
+		'DVD',
+		'Electronics',
+		'Grocery',
+		'HealthPersonalCare',
+		'HomeGarden',
+		'HomeImprovement',
+		'Jewelry',
+		'KindleStore',
+		'Kitchen',
+		'Lighting',
+		'Marketplace',
+		'MP3Downloads',
+		'Music',
+		'MusicTracks',
+		'MusicalInstruments',
+		'OfficeProducts',
+		'OutdoorLiving',
+		'Outlet',
+		'PetSupplies',
+		'PCHardware',
+		'Shoes',
+		'Software',
+		'SoftwareVideoGames',
+		'SportingGoods',
+		'Tools',
+		'Toys',
+		'VHS',
+		'Video',
+		'VideoGames',
+		'Watches'
+		],
+	
+	
+	
+];
