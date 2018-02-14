c92e7e2187ccb87a2d00162d8b61ce08d5ecbc95
gutropolis <gutropolis@gmail.com>
2018-02-14 12:59:19 +0530

    product api

--- resources/assets/js/views/pages/manage_seller/seller_product.vue
+++ resources/assets/js/views/pages/manage_seller/seller_product.vue
@@ -12,50 +12,64 @@
                         <div class="dashboard__content--description">
                             <hr>
                             <h3 class="box-title m-b-20">Add Product</h3>
-                       	<div class="form-group ">
-                        <label class="login__element--box--label">Choose Image</label>
-                           <input type="file" name="image" v-on:change="onFileChange"   class="form-control">
-                        <div class="col-md-2">
-						<img :src="image" class="img-responsive">
-						</div>
-                    </div>
-					<div class="col-md-2">
-					<button class="btn btn-success btn-block" v-if="image" @click="upload">Upload</button>
-					</div>
+                       	
+					
 					    
-                     <form class="form-horizontal form-material" id="saveproductform" @submit.prevent="submit"  enctype="multipart/form-data">
+						
+                     <form class="form-horizontal form-material" id="productform" @submit.prevent="submit1">
                     
 					
 				
 					<div class="form-group ">
-                        <label class="login__element--box--label">Title</label>
-                            <input type="text" name="title" class="login__element--box--input"  placeholder="Title" v-model="saveproductform.title">
+                        <label class="login__element--box--label">Keywords</label>
+                            <input type="text" name="keywords" class="login__element--box--input"  placeholder="Keywords" v-model="productform.keywords">
                       
                     </div>
+					
+		
+					
+					 <div class="form-group text-center">
+                            <input type="Submit" placeholder="" value="Search" class="btn btn-bg-orange login__element--box--button">
+                        </div>
+					</form>
+			
+				  <form class="form-horizontal form-material" id="items" @submit.prevent="submit">
+				<div class="form-group ">
+                        <label class="login__element--box--label">Title</label>
+                            <input type="text" name="title" class="login__element--box--input"  placeholder="Title" v-model="items.name">
+                      
+                    </div>
+					<input type="hidden" name="organisation_id" v-model="items.organisation_id"   class="login__element--box--input" />
                     <div class="form-group ">
-                        <label class="login__element--box--label">Description</label>
-                            <input type="text" name="description" class="login__element--box--input"   placeholder="Description" v-model="saveproductform.description">
+                        <label class="login__element--box--label">ASIN</label>
+                            <input type="text" name="asin" class="login__element--box--input"   placeholder="Description" v-model="items.ASIN">
                        
                     </div>
 					<div class="form-group ">
-                        <label class="login__element--box--label">Asin_url</label>
-                            <input type="text" name="asin_url" class="login__element--box--input"  placeholder="asin_url" v-model="saveproductform.asin_url">
+                        <label class="login__element--box--label">Description</label>
+                            <input type="text" name="description" class="login__element--box--input"   placeholder="Description" v-model="items.description">
                        
                     </div>
-					
-                   
-					 
 					 <div class="form-group ">
                        <label class="login__element--box--label">Reviews</label>
-                            <input type="text" name="reviews" class="login__element--box--input" placeholder="reviews" v-model="saveproductform.reviews">
+                            <input type="text" name="reviews" class="login__element--box--input" placeholder="reviews" v-model="items.reviews">
                         
                     </div>
-					
-					
-					 <div class="form-group text-center">
+					 <div class="form-group ">
+                       <label class="login__element--box--label">Image</label>
+                            <input type="text" name="image" class="login__element--box--input" placeholder="image" v-model="items.image">
+                        
+                    </div>
+					<div class="form-group ">
+                        <label class="login__element--box--label">Units</label>
+                            <input type="text" name="units" class="login__element--box--input"  placeholder="units" v-model="items.units">
+                       
+                    </div>
+					<div class="form-group text-center">
                             <input type="Submit" placeholder="" value="Save" class="btn btn-bg-orange login__element--box--button">
                         </div>
-					</form>
+				</form>
+					
                         </div>
                     </div>
                 </div>
@@ -63,67 +77,51 @@
 			
         </div>
     </section>
+	{{items|json}}
 	
-
 </div>
 </template>
 <script>
-import AppNavbar from '../users/navbar.vue' 
+
  import AppSidebar from '../users/sidebar.vue'
  export default {
   components: {
-            AppNavbar,  AppSidebar 
+            AppSidebar 
         },
         data() {
             return {
-			image: '',
+					items:[],
+						productform:{
+						 keywords: '',
+						},
 				
-                saveproductform: {
-                    title: '',
-                    description: '',
-					image:'',
-                  
-                    
-                }
-            }
-        },
-       
+               
+        }
+    },
         
         mounted(){
         },
         methods: {
-		onFileChange(e) {
-                let files = e.target.files || e.dataTransfer.files;
-                if (!files.length)
-                    return;
-                this.createImage(files[0]);
-            },
-            createImage(file) {
-                let reader = new FileReader();
-                let vm = this;
-                reader.onload = (e) => {
-                    vm.image = e.target.result;
-                };
-                reader.readAsDataURL(file);
-            },
-		upload(){
-                axios.post('/api/product_upload',{image: this.image}).then(response => {
-					 toastr['success'](response.data.message);
+		
+			
+            submit1(e){
+                axios.post('/api/product/search',this.productform).then(response =>  {
+                   this.items=response.data;
+				 
+                    this.$router.push('/product');
+                }).catch(error => {
+                    toastr['error'](error.response.data.message);
                 });
-				},
-            submit(e){
-                axios.post('/api/gs_seller_product',this.saveproductform).then(response =>  {
+            },
+			 submit(e){
+                axios.post('/api/gs_seller_product',this.items).then(response =>  {
                     toastr['success'](response.data.message);
                     this.$router.push('/product_list');
                 }).catch(error => {
                     toastr['error'](error.response.data.message);
                 });
             },
-			
-			
-            
-           
-            
+			   
         }
     }
 </script>
