f3a0c131a9e4eff5dae040720974f8e6a0f6f7a7
gutropolis <gutropolis@gmail.com>
2018-02-21 16:20:44 +0530

    Admin changes

--- app/Seller.php
+++ app/Seller.php
@@ -8,7 +8,7 @@ class Seller extends Model
 {
  protected $table="gs_vender_organisation";
     protected $fillable = [
-        'title','description','location','pic','year_in_business','start_up_year','address','mission_statement','vision_statement','tax_id','user_id','phone_number','business_type','post_type'
+        'title','description','location','pic','year_in_business','keywords','start_up_year','address','mission_statement','vision_statement','tax_id','user_id','phone_number','business_type','post_type'
     ];
 
 			public function author()
