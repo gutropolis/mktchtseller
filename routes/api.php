2a814e0015ade17fbff9ce9ca5c1de1db7aa26f4
gutropolis <gutropolis@gmail.com>
2018-02-21 19:14:48 +0530

    seller change

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::post('/selectrole','UserController@role');
 });
 
 Route::group(['middleware' => ['jwt.auth']], function () {
+	
 //user Profile
 Route::get('/auth/user','AuthController@getAuthUser');
 Route::post('/user/update-profile','UserController@updateProfile');
@@ -34,6 +35,9 @@ Route::post('/user/update-avatar','UserController@updateAvatar');
 Route::post('/user/remove-avatar','UserController@removeAvatar');
 Route::get('/get_user','UserController@index');
 Route::post('/user/change-password','AuthController@changePassword');
+
+
+
 //seller Product
 Route::delete('/task/{id}','SellerproductController@destroy');
 Route::post('/task/{id}','SellerproductController@update');
@@ -41,6 +45,9 @@ Route::get('/task/{id}','SellerproductController@edit');
 Route::get('/product_list','SellerproductController@index');
 Route::post('product/update-avatar','SellerproductController@updateAvatar');
 Route::post('product/remove-avatar','SellrproductController@removeAvatar');
+Route::resource('/gs_seller_product', 'SellerproductController');
+Route:: post('/gs_seller_product', 'SellerproductController@store');
+
 
 //Seller Organisation
 Route::resource('/gs_seller_organisation', 'SellerController');
@@ -48,10 +55,12 @@ Route::get('/gs_seller_organisation', 'SellerController@index');
 Route::get('/seller_list','SellerController@seller_list');
 Route::get('/edit_seller/{id}','SellerController@edit');
 Route::post('/edit_seller/{id}','SellerController@update');
+Route::post('/update-seller/{id}','SellerController@updateSeller' );
 Route::delete('/seller_list/{id}','SellerController@destroy');
-
 Route::get('/vender_category','SellerCategoryController@index');
 Route::post('/vender_category','SellerCategoryController@store');
+
+//Charity Ads
 Route::post('/create_ads','AdsController@store');
 Route::get('/charityads/','AdsController@index');
 Route :: get('/charityads/{id}','AdsController@edit');
@@ -59,35 +68,32 @@ Route::delete('/charityads/{id}','AdsController@destroy');
 Route :: post('/charityads/{id}','AdsController@update');
 Route::post('/update-avatar/{id}','AdsController@updateAvatar' );
 Route::post('/update-image','AdsController@updateImage' );
-Route::resource('/gs_seller_product', 'SellerproductController');
-Route:: post('/gs_seller_product', 'SellerproductController@store');
 
-//message communicate
-Route::post('/create_messages','MessagesellerController@store');
-});
-//Route::get('/product/search', 'ProductController@getSearchPage');
-Route::get('/product/search','ProductController@formsearch');
-Route::post('/product/search', 'ProductController@formSearch');
-
-
-Route::resource('/gs_charity_organisation', 'charityController');
-//Charity Organisation 
-Route::get('/charity_details/{id}','charityController@charity_details');
-Route::post('/search','charityController@search');
-Route::get('/search','charityController@search');
-
-Route::get('/productsearch', 'SellerproductController@search');
-Route::post('/productsearch', 'SellerproductController@search');
-Route::get('/product_details/{id}','SellerproductController@product_details');
-
+//Charity organisation
 Route::get('/charity_organisation','charityController@index');
 Route::post('/charity_category','CharityCategoryController@store');
 Route::get('/charity_category','CharityCategoryController@index');
 Route::get('/charity_list','charityController@charity_list');
 Route::get('/edit_charity/{id}','charityController@edit');
+Route::post('/update-charity/{id}','charityController@updateCharity' );
 Route::post('/edit_charity/{id}','charityController@update');
 Route::delete('/charity_list/{id}','charityController@destroy');
-//Route::resource('/edit_charity', 'charityController');
+Route::resource('/gs_charity_organisation', 'charityController');
+
+});
+
+
+//Route::get('/product/search', 'ProductController@getSearchPage');
+Route::get('/product/search','ProductController@formsearch');
+Route::post('/product/search', 'ProductController@formSearch');
+Route::get('/product_details/{id}','SellerproductController@product_details');
+
+
+//charity list
+Route::get('/charity_details/{id}','charityController@charity_details');
+Route::post('/search','charityController@search');
+Route::get('/search','charityController@search');
+
 
 //message part
 Route::get('/create_message','MessageController@index');
