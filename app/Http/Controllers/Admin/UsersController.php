7eb7b0c58594523f0cdc42630cc9d9b4a70d7eb7
gutropolis <gutropolis@gmail.com>
2018-02-21 16:18:25 +0530

    Admin changes

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -1,7 +1,7 @@
 <?php namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\JoshController;
-use App\Http\Requests;
+use App\Http\Requests\UserRequest;
 use App\Users;
 use Cartalyst\Sentinel\Laravel\Facades\Activation;
 use File;
@@ -88,17 +88,10 @@ class UsersController extends JoshController
      *
      * @return Redirect
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
         $data = new stdClass();
-        //upload image
-        if ($file = $request->file('pic_file')) {
-            $extension = $file->extension()?: 'png';
-            $destinationPath = public_path() . '/uploads/users/';
-            $safeName = str_random(10) . '.' . $extension;
-            $file->move($destinationPath, $safeName);
-            $request['pic'] = $safeName;
-        }
+       
         //check whether use should be activated by default or not
         $activate = $request->get('activate') ? true : false;
 
@@ -106,29 +99,41 @@ class UsersController extends JoshController
             // Register the user
             $user = Sentinel::register($request->except('_token', 'password_confirm', 'group', 'activate', 'pic_file'), $activate);
 
-            //add user to 'User' group
-            $role = Sentinel::findRoleById($request->get('group'));
-            if ($role) {
-                $role->users()->attach($user);
-            }
-            //check for activation and send activation mail if not activated by default
-            if (!$request->get('activate')) {
-                // Data to be used on the email view
-                $data->user_name =$user->first_name .' '. $user->last_name;
-                $data->activationUrl = URL::route('activate', [$user->id, Activation::create($user)->code]);
-
-                // Send the activation code through email
-                Mail::to($user->email)
-                    ->send(new Restore($data));
-            }
-            // Activity log for New user create
+            //upload image
+        if ($file = $request->file('pic_file')) {
+            $extension = $file->extension()?: 'png';
+            $destinationPath = public_path() . '/images/user/';
+            $safeName = str_random(10) . '.' . $extension;
+            $file->move($destinationPath, $safeName);
+            $request['pic'] = $safeName;
+			 $user->avatar=  $request['pic'];
+        }
+		
+			$selectedRoles = $request->group;
+			$user->dob= $request->dob;
+			$user->bio = $request->bio;
+			$user->gender = $request->gender;
+			$user->country = $request->country;
+			$user->state= $request->state;
+			$user->city = $request->city;
+			$user->address = $request->address;
+			$user->postal = $request->postal;
+		
+			$user->role=$selectedRoles;
+			$user->status='activated';
+		
+			$user->save();
+           
+			if ($user->save()) {
+                // Prepare the success message
+                $success =trans('users/message.success.create');
             activity($user->full_name)
                 ->performedOn($user)
                 ->causedBy($user)
                 ->log('New User Created by '.Sentinel::getUser()->full_name);
             // Redirect to the home page with success menu
             return Redirect::route('admin.users.index')->with('success', trans('users/message.success.create'));
-
+			}
         } catch (LoginRequiredException $e) {
             $error = trans('admin/users/message.user_login_required');
         } catch (PasswordRequiredException $e) {
@@ -152,7 +157,7 @@ class UsersController extends JoshController
 
         // Get this user groups
         $userRoles = $user->getRoles()->pluck('name', 'id')->all();
-        // Get a list of all the available groups
+        
         $roles = Sentinel::getRoleRepository()->all();
 
         $status = Activation::completed($user);
@@ -170,7 +175,7 @@ class UsersController extends JoshController
      * @param UserRequest $request
      * @return Redirect
      */
-    public function update(Users $user, Request $request)
+    public function update(Users $user, UserRequest $request)
     {
         $data = new stdClass();
 
@@ -180,74 +185,47 @@ class UsersController extends JoshController
             if ( !empty($request->password)) {
                 $user->password = Hash::make($request->password);
             }
+			
 
             // is new image uploaded?
             if ($file = $request->file('pic_file')) {
                 $extension = $file->extension()?: 'png';
-                $destinationPath = public_path() . '/uploads/users/';
+                $destinationPath = public_path() . '/images/user/';
                 $safeName = str_random(10) . '.' . $extension;
                 $file->move($destinationPath, $safeName);
+				 $request['images'] = $safeName;
                 //delete old pic if exists
-                if (File::exists($destinationPath . $user->pic)) {
-                    File::delete($destinationPath . $user->pic);
+                if (File::exists($destinationPath . $user->avatar)) {
+                    File::delete($destinationPath . $user->avatar);
                 }
                 //save new file path into db
-                $user->pic = $safeName;
+                	$user->avatar=  $request['images'];
             }
+			
+			$selectedRoles = $request->group;
+			$user->dob= $request->dob;
+			$user->bio = $request->bio;
+			$user->gender = $request->gender;
+			$user->country = $request->country;
+			$user->state= $request->state;
+			$user->city = $request->city;
+			$user->address = $request->address;
+			$user->postal = $request->postal;
+		
+			$user->role=$selectedRoles;
+			$user->status='activated';
 
-            //save record
             $user->save();
 
-            // Get the current user groups
+           
             $userRoles = $user->roles()->pluck('id')->all();
 
-            // Get the selected groups
+     
 
             $selectedRoles = $request->get('groups');
 
-            // Groups comparison between the groups the user currently
-            // have and the groups the user wish to have.
-            $rolesToAdd = array_diff($selectedRoles, $userRoles);
-            $rolesToRemove = array_diff($userRoles, $selectedRoles);
-
-            // Assign the user to groups
-
-            foreach ($rolesToAdd as $roleId) {
-                $role = Sentinel::findRoleById($roleId);
-                $role->users()->attach($user);
-            }
-
-            // Remove the user from groups
-            foreach ($rolesToRemove as $roleId) {
-                $role = Sentinel::findRoleById($roleId);
-                $role->users()->detach($user);
-            }
-
-            // Activate / De-activate user
-
-            $status = $activation = Activation::completed($user);
-
-            if ($request->get('activate') != $status) {
-                if ($request->get('activate')) {
-                    $activation = Activation::exists($user);
-                    if ($activation) {
-                        Activation::complete($user, $activation->code);
-                    }
-                } else {
-                    //remove existing activation record
-                    Activation::remove($user);
-                    //add new record
-                    Activation::create($user);
-                    //send activation mail
-                    $data->user_name =$user->first_name .' '. $user->last_name;
-                    $data->activationUrl = URL::route('activate', [$user->id, Activation::exists($user)->code]);
-                    // Send the activation code through email
-                    Mail::to($user->email)
-                        ->send(new Restore($data));
-
-                }
-            }
-
+           
+			
             // Was the user updated?
             if ($user->save()) {
                 // Prepare the success message
@@ -283,7 +261,7 @@ class UsersController extends JoshController
     public function getDeletedUsers()
     {
         // Grab deleted users
-        $users = Users::onlyTrashed()->get();
+        $users = User::onlyTrashed()->get();
 
         // Show the page
         return view('admin.deleted_users', compact('users'));
@@ -340,7 +318,7 @@ class UsersController extends JoshController
             }
             // Delete the user
             //to allow soft deleted, we are performing query on users model instead of Sentinel model
-            Users::destroy($id);
+            User::destroy($id);
             Activation::where('user_id',$user->id)->delete();
             // Prepare the success message
             $success = trans('users/message.success.delete');
