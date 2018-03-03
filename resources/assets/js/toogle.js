5e23fc3501f0b28c36da07187c26a4cdf09c297d
gutropolisdev <gutropolis.dev@gmail.com>
2018-03-03 10:59:45 +0530

    toogle js

---
+++ resources/assets/js/toogle.js
@@ -0,0 +1,7 @@
+    $(document).ready(function(){
+        $(".detail_slide-toggle").click(function(){
+            $(".mobile_message_outer").animate({
+                width: "toggle"
+            });
+        });
+    });
