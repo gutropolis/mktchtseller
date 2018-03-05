bb77c12a2a0d268ca36c63c2b13bfe891fae6275
gutropolis <gutropolis@gmail.com>
2018-03-05 12:01:35 +0530

    change

--- app/Http/Controllers/charityController.php
+++ app/Http/Controllers/charityController.php
@@ -75,7 +75,7 @@ class charityController extends JoshController
 			'description'=>'required',
 			'location'=>'required',
 			'year_in_business'=>'required',
-			'start_up_year'=>'required',
+			
 			'business_purpose'=>'required',
 			'address'=>'required',
 			'phone_number'=>'required',
@@ -92,7 +92,7 @@ class charityController extends JoshController
 			'description'=> request('description'),
 			'location'=> request('location'),
 			'year_in_business'=> request('year_in_business'),
-			'start_up_year'=> request('start_up_year'),
+			
 			'business_purpose'=> request('business_purpose'),
 			'address'=> request('address'),
 			'phone_number'=> request('phone_number'),
