a2422249d0181ee75514ee13e81a8156f826ecc0
gutropolis <gutropolis@gmail.com>
2018-03-14 13:09:00 +0530

    chnages

--- routes/channels.php
+++ routes/channels.php
@@ -12,8 +12,8 @@
 */
 
 Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return true;
 });
 Broadcast::channel('chat', function ($user) {
-  return Auth::check();
+  return true;
 });
