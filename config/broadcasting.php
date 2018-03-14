fb1e01a052ed35205dd676abc612a7c3ec851c1c
gutropolis <gutropolis@gmail.com>
2018-03-14 09:58:35 +0530

    changd

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -30,15 +30,16 @@ return [
 
     'connections' => [
 
-        'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                //
-            ],
-        ],
+      'pusher' => [
+      'driver' => 'pusher',
+      'key' => env('PUSHER_APP_KEY'),
+      'secret' => env('PUSHER_APP_SECRET'),
+      'app_id' => env('PUSHER_APP_ID'),
+      'options' => [
+          'cluster' => 'ap2',
+          'encrypted' => true,
+      ],
+  ],
 
         'redis' => [
             'driver' => 'redis',
