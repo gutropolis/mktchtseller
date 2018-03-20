9f2be7546dc72c76adffe0156c74ca44bf93f624
gutropolis <gutropolis@gmail.com>
2018-03-20 16:58:33 +0530

    detail

--- resources/assets/js/views/pages/my_ads.vue
+++ resources/assets/js/views/pages/my_ads.vue
@@ -32,7 +32,7 @@
                             <div class="notification__element-bottom">
                                <div class="notification__element--mark">
                                     
-                                    <input type='checkbox' class="notification__element--mark--value" @click='checkAll()' v-model='isCheckAll'> Mark All 
+                                   
                                 </div> 
                                 <div class="notification__element--button clearfix">
                                     <!-- <button type="button" class="btn">Mark as Read</button> -->
@@ -52,42 +52,27 @@
 </g>
 
 </svg>Create Ads</b-button>
-                                    <b-button to="/login" class="btn"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                    width="408.483px" height="408.483px" viewBox="0 0 408.483 408.483" style="enable-background:new 0 0 408.483 408.483;"
-                                    xml:space="preserve">
-                                   <g>
-                                       <path d="M87.748,388.784c0.461,11.01,9.521,19.699,20.539,19.699h191.911c11.018,0,20.078-8.689,20.539-19.699l13.705-289.316
-                                           H74.043L87.748,388.784z M247.655,171.329c0-4.61,3.738-8.349,8.35-8.349h13.355c4.609,0,8.35,3.738,8.35,8.349v165.293
-                                           c0,4.611-3.738,8.349-8.35,8.349h-13.355c-4.61,0-8.35-3.736-8.35-8.349V171.329z M189.216,171.329
-                                           c0-4.61,3.738-8.349,8.349-8.349h13.355c4.609,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.737,8.349-8.349,8.349h-13.355
-                                           c-4.61,0-8.349-3.736-8.349-8.349V171.329L189.216,171.329z M130.775,171.329c0-4.61,3.738-8.349,8.349-8.349h13.356
-                                           c4.61,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.738,8.349-8.349,8.349h-13.356c-4.61,0-8.349-3.736-8.349-8.349V171.329z"/>
-                                       <path d="M343.567,21.043h-88.535V4.305c0-2.377-1.927-4.305-4.305-4.305h-92.971c-2.377,0-4.304,1.928-4.304,4.305v16.737H64.916
-                                           c-7.125,0-12.9,5.776-12.9,12.901V74.47h304.451V33.944C356.467,26.819,350.692,21.043,343.567,21.043z"/>
-                                   </g>
-                               
-                               </svg>Delete</b-button>
+                                    
 							 
                                 </div>
                             </div>
 								<div v-for="item in items">
                             <div class="notification__element--view">
 						
-                                <div class="notification__element--mark">
-                                    <input type="checkbox" value="mark" class="notification__element--mark--check">
-                                </div>
+                               
                                 <div class="notification__element--listing">
                                     <figure class="notification__element--listing--figure">
-                                        <img src="images/download.jpg" class="notification__element--listing--figure--image">
+                                       <img :src="'/images/charityads/'+ item.image" class="notification__element--listing--figure--image">
                                     </figure>
                                     <div class="notification__element--listing--content">
-                                        <router-link :to="{name: 'edit_ads', params: { id: item.id }}" ><h4 class="charity__listing--content--box--heading">{{item.title}}</h4></router-link>
+                                        <h4 class="charity__listing--content--box--heading">{{item.title}}</h4>
                                         <p>{{item.description}}</p>
-                                        <p><strong>Like:</strong>{{item.like}}<strong>Location:</strong>{{item.location}}</p>
+                                      <p><b>Location:</b> {{item.location}}</p>
                                     </div>
                                 </div>
                                 <div class="notification__element--view--button">
-                                    <button class="btn notification__element--view--button--content btn-border-orange">View More</button>
+                                     <router-link :to="{name: 'edit_ads', params: { id: item.id }}" class="btn notification__element--view--button--content btn-border-orange">Edit</router-link>
+									<button class="btn notification__element--view--button--content btn-border-orange"  v-on:click="deleteItem(item.id)">Delete</button>
                                 </div>
                             </div>
 							</div>
@@ -132,8 +117,8 @@ import AppNavbar from './users/navbar.vue'
       
       },
     
-		deleteTask(item){
-                axios.delete('/api/get_ad/'+item.id).then(response => {
+		 deleteItem(id){
+                axios.delete('/api/charityads/'+id).then(response => {
                     toastr['success'](response.data.message);
 					this.fetchItems();
                     
@@ -143,7 +128,7 @@ import AppNavbar from './users/navbar.vue'
             },
                fetchItems()
 		 {
-              axios.get('/api/get_ad').then((response) => {
+              axios.get('/api/charityads').then((response) => {
                  this.items=response.data;
 				 this.loaded = true;
 					
