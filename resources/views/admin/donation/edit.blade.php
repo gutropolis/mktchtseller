47c7b11ffe27d3646b2d350588319038adb12b3e
gutropolis <gutropolis@gmail.com>
2018-03-20 17:05:26 +0530

    add files

---
+++ resources/views/admin/donation/edit.blade.php
@@ -0,0 +1,146 @@
+@extends('admin/layouts/default')
+{{-- Web site Title --}}
+@section('title')
+@lang('admin/edit')
+@parent
+@stop
+{{-- Content --}}
+@section('content')
+<section class="content-header">
+   <h1>
+      @lang('Donation Edit')
+   </h1>
+   <ol class="breadcrumb">
+      <li>
+         <a href="{{ route('admin.dashboard') }}">
+         <i class="livicon" data-name="home" data-size="14" data-color="#000"></i>
+         @lang('general.dashboard')
+         </a>
+      </li>
+      <li>@lang('Donation Edit')</li>
+      <li class="active">
+         @lang('Donation.edit')
+      </li>
+   </ol>
+</section>
+<!-- Main content -->
+<section class="content">
+   <div class="row">
+      <div class="col-lg-12">
+         <div class="panel panel-primary ">
+            <div class="panel-heading">
+               <h4 class="panel-title"> <i class="livicon" data-name="users-add" data-size="16" data-loop="true" data-c="#fff" data-hc="white"></i>
+                  @lang('donation.edit')
+               </h4>
+            </div>
+            <div class="panel-body">
+               {!! $errors->first('slug', '<span class="help-block">Another role with same slug exists, please choose another name</span> ') !!}
+               @if (count($errors) > 0)
+               <div class="alert alert-danger">
+                  <ul>
+                     @foreach ($errors->all() as $error)
+                     <li>{{ $error }}</li>
+                     @endforeach
+                  </ul>
+               </div>
+               @endif
+               @foreach($donation as $rol) @endforeach
+               {!! Form::model($rol, ['url' => URL::to('admin/donation/'. $rol->id), 'method' => 'put', 'class' => 'form-horizontal']) !!}
+               <!-- CSRF Token -->
+               {{ csrf_field() }}
+              
+               <div class="form-group {{ $errors->
+                  first('name', 'has-error') }}">
+                  <label for="seller" class="col-sm-2 control-label">
+                  @lang('Seller Name')
+                  </label>
+                  <div class="col-sm-5">
+                     <input type="text" name="seller" id="seller" required class="form-control resize_vertical"
+                        rows="4" value="{!! old('seller'),$rol->seller !!}">
+                  </div>
+                  <div class="col-sm-4">
+                     {!! $errors->first('seller', '<span class="help-block">:message</span> ') !!}
+                  </div>
+               </div>
+                <div class="form-group {{ $errors->first('product', 'has-error') }}">
+                  <label for="Product" class="col-sm-2 control-label">Select Product *</label>
+                  <div class="col-sm-10">
+                     <select name="product" class="form-control required" required>
+                        <option value="{{$rol->product}}">{{$rol->product}}</option>
+                        @foreach($product as $sellerproduct)
+                        <option value="{{$sellerproduct->title}}">--{{$sellerproduct->title}}</option>
+                        @endforeach  
+                     </select>
+                     {!! $errors->first('product', '<span class="help-block">:message</span>') !!}
+                  </div>
+               </div>
+               <div class="form-group {{ $errors->
+                  first('name', 'has-error') }}">
+                  <label for="Seller" class="col-sm-2 control-label">
+                  @lang('Charity Owner')
+                  </label>
+                  <div class="col-sm-5">
+                     <input type="text" name="charity" id="charity" required class="form-control resize_vertical"
+                        rows="4" value="{!! old('charity'),$rol->owner_charity !!}">
+                  </div>
+                  <div class="col-sm-4">
+                     {!! $errors->first('charity', '<span class="help-block">:message</span> ') !!}
+                  </div>
+               </div>
+               <div class="form-group {{ $errors->first('charity_organisation', 'has-error') }}">
+                  <label for="Charity" class="col-sm-2 control-label">Select Charity *</label>
+                  <div class="col-sm-10">
+                     <select name="charity_organisation" class="form-control required" required>
+                        <option value="{{$rol->charity_organisation}}">{{$rol->charity_organisation}}</option>
+                        @foreach($charity as $charity_organsation)
+                        <option value="{{$charity_organsation->title}}">--{{$charity_organsation->title}}</option>
+                        @endforeach  
+                     </select>
+                     {!! $errors->first('charity_organisation', '<span class="help-block">:message</span>') !!}
+                  </div>
+               </div>
+               <div class="form-group {{ $errors->
+                  first('name', 'has-error') }}">
+                  <label for="Units" class="col-sm-2 control-label">
+                  @lang('Units')
+                  </label>
+                  <div class="col-sm-5">
+                     <input type="text" name="units" id="units" required class="form-control resize_vertical"
+                        rows="4" value="{!! old('units'),$rol->units !!}">
+                  </div>
+                  <div class="col-sm-4">
+                     {!! $errors->first('units', '<span class="help-block">:message</span> ') !!}
+                  </div>
+               </div>
+               <div class="form-group {{ $errors->first('status', 'has-error') }}">
+                  <label for="Status" class="col-sm-2 control-label">Status *</label>
+                  <div class="col-sm-10">
+                     <select name="status" class="form-control required" required>
+                        <option value="{{$rol->status}}">{{$rol->status}}</option>
+                     
+                        <option value="pending">--Pending</option>
+                         <option value="in progress">--In Progress</option>
+                          <option value="completed">--Completed</option>
+                       
+                     </select>
+                     {!! $errors->first('product', '<span class="help-block">:message</span>') !!}
+                  </div>
+               </div>
+               <div class="form-group">
+                  <div class="col-sm-offset-2 col-sm-4"> 
+                     <button type="submit" class="btn btn-success">
+                     @lang('Update Donation')
+                     </button>
+                     <a class="btn btn-danger" href="{{ route('admin.cms.index') }}">
+                     @lang('Cancel')
+                     </a>
+                  </div>
+               </div>
+               </form>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!-- row-->
+</section>
+@stop
---
+++ resources/views/admin/donation/index.blade.php
@@ -0,0 +1,112 @@
+@extends('admin/layouts/default')
+
+{{-- Page title --}}
+@section('title')
+Seller List
+@parent
+@stop
+
+{{-- page level styles --}}
+@section('header_styles')
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/datatables/css/dataTables.bootstrap.css') }}" />
+<link href="{{ asset('assets/css/pages/tables.css') }}" rel="stylesheet" type="text/css" />
+@stop
+
+
+{{-- Page content --}}
+@section('content')
+<section class="content-header">
+    <h1>Seller</h1>
+    <ol class="breadcrumb">
+        <li>
+           <!-- <a href="{{ route('admin.dashboard') }}">-->
+           <a href="">
+                <i class="livicon" data-name="home" data-size="14" data-color="#000"></i>
+                Dashboard
+            </a>
+        </li>
+        <li><a href="#"> Seller</a></li>
+        <li class="active">Seller List</li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content paddingleft_right15">
+    <div class="row">
+        <div class="panel panel-primary ">
+            <div class="panel-heading">
+                <h4 class="panel-title"> <i class="livicon" data-name="seller" data-size="16" data-loop="true" data-c="#fff" data-hc="white"></i>
+                    Seller List
+                </h4>
+            </div>
+            <br />
+            <div class="panel-body">
+                <div class="table-responsive">
+                <table class="table table-bordered width100" id="table">
+                    <thead>
+                        <tr class="filters">
+                           <th>Id</th>
+                                <th>Seller Name</th>
+                                <th>Donated Product</th>
+                                <th>Units</th>
+                                <th>Charity Name</th>
+                                <th>Created At</th>
+                                <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+
+                    </tbody>
+                </table>
+                </div>
+            </div>
+        </div>
+    </div>    <!-- row-->
+</section>
+@stop
+
+{{-- page level scripts --}}
+@section('footer_scripts')
+    <script type="text/javascript" src="{{ asset('assets/vendors/datatables/js/jquery.dataTables.js') }}" ></script>
+    <script type="text/javascript" src="{{ asset('assets/vendors/datatables/js/dataTables.bootstrap.js') }}" ></script>
+
+<script>
+    $(function() {
+        var table = $('#table').DataTable({
+            processing: true,
+            serverSide: true,
+              ajax: '{!! route('admin.donation_list.data') !!}',
+            columns: [
+                { data: 'id', name: 'id' },
+            { data: 'seller', name: 'Seller Name' },
+            { data: 'product', name: 'Donated Product' },
+            {data: 'units', name: 'units'},
+            {data:'charity_organisation', name: 'Charity Name'},
+            { data: 'created_at', name:'created_at'},
+                
+                { data: 'actions', name: 'actions', orderable: false, searchable: false }
+            ]
+        });
+        table.on( 'draw', function () {
+            $('.livicon').each(function(){
+                $(this).updateLivicon();
+            });
+        } );
+    });
+
+</script>
+
+<div class="modal fade" id="delete_confirm" tabindex="-1" role="dialog" aria-labelledby="seller_delete_confirm_title" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content"></div>
+  </div>
+</div>
+<script>
+$(function () {
+    $('body').on('hidden.bs.modal', '.modal', function () {
+        $(this).removeData('bs.modal');
+    });
+});
+</script>
+@stop
