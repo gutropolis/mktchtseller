79ea00c09d648ff8d94abacb1ac2a92e2b52b06a
gutropolis <gutropolis@gmail.com>
2018-03-29 10:40:37 +0530

    edit seller

--- resources/assets/js/views/pages/manage_seller/edit_seller.vue
+++ resources/assets/js/views/pages/manage_seller/edit_seller.vue
@@ -30,7 +30,7 @@
                         </div>
                         <div class="form-group">
                             <label class="login__element--box--label">Description</label>
-                            <input type="text" placeholder="Description" v-model="items.description"  class="login__element--box--input">
+                           <textarea  type="text" v-model="items.description" rows="5" placeholder="Description" class="login__element--box--input"></textarea>
                         </div>
 						  <div class="form-group">
                             <label class="login__element--box--label">Location</label>
@@ -45,8 +45,12 @@
 						
 						
 						<div class="form-group">
-                            <label class="login__element--box--label">Address</label>
-                             <textarea  type="text" rows="3" v-model="items.address" placeholder="Address" class="login__element--box--input"></textarea>
+                            <label class="login__element--box--label">State</label>
+                             <input  type="text"  v-model="items.state" placeholder="Fill_state" class="login__element--box--input">
+                        </div>
+						<div class="form-group">
+                            <label class="login__element--box--label">ZipCode</label>
+                             <input  type="text"  v-model="items.zipcode" placeholder="Fill_Zipcode" class="login__element--box--input">
                         </div>
 						<div class="form-group">
                             <label class="login__element--box--label">Phone Number</label>
@@ -55,8 +59,8 @@
 						</tab-content>
 						 <tab-content>
 						<div class="form-group">
-                            <label class="login__element--box--label">Keyword</label>
-                            <input type="text" placeholder="keywords" v-model="items.keywords" class="login__element--box--input">
+                            <label class="login__element--box--label">Website</label>
+                            <input type="text" placeholder="website" v-model="items.website" class="login__element--box--input">
                         </div>
 						<div class="form-group">
                             <label class="login__element--box--label">Vision</label>
@@ -70,6 +74,21 @@
                             <label class="login__element--box--label">Tax_Id</label>
                             <input type="text" placeholder="Tax_id" v-model="items.tax_id" class="login__element--box--input">
                         </div>
+						 <h4 class="card-title">Upload Logo</h4>
+                                       <div class="form-group text-center m-t-20">
+                                          <span id="fileselector">
+                                          <label class="btn btn-info">
+                                          <input type="file"  @change="previewAvatar" id="avatarUpload" class="upload-button">
+                                          </label>
+                                          </span>
+                                       </div>
+                                       <div class="form-group text-center">
+                                          <img :src="avatar" class="img-responsive" style="max-width:200px;">
+                                       </div>
+                                       <div class="text-center">
+                                          <button type="submit" class="btn btn-info waves-effect waves-light m-t-10" v-if="avatar" @click="uploadAvatar">Upload</button>
+                                          <button type="button" class="btn btn-danger waves-effect waves-light m-t-10" v-if="avatar" @click="cancelUploadAvatar">Cancel Upload</button>
+                                       </div>
 						
                     </tab-content>
 					</form-wizard>
@@ -99,6 +118,7 @@ import Vue from 'vue'
   
         data() {
             return {
+			avatar: '',
 			items:{},
 			category:[],
                     
@@ -114,6 +134,37 @@ import Vue from 'vue'
         mounted(){
         },
         methods: {
+		 previewAvatar(e) {
+                let files = e.target.files || e.dataTransfer.files;
+                if (!files.length)
+                    return;
+                this.createAvatar(files[0]);
+            },
+            createAvatar(file) {
+                let reader = new FileReader();
+                reader.onload = (e) => {
+                    this.avatar = e.target.result;
+                };
+                reader.readAsDataURL(file);
+            },
+            cancelUploadAvatar(){
+                this.avatar = '';
+            },
+            uploadAvatar() {
+                let data = new FormData();
+                data.append('avatar', $('#avatarUpload')[0].files[0]);
+                axios.post('/api/update-seller_logo/'+this.$route.params.id,data)
+                .then(response => {
+                  
+                 
+                    toastr['success'](response.data.message);
+                    this.avatar = '';
+                    $("#avatarUpload").val('');
+                }).catch(error => {
+                    toastr['error'](error.response.data.message);
+                });
+            },
+           
            
 			fetchItems()
 			{
@@ -127,6 +178,7 @@ import Vue from 'vue'
 			},
 			 updateitems()
             {
+			
               axios.post('api/edit_seller/'+this.$route.params.id,this.items).then(response =>  {
                     toastr['success'](response.data.message);
                     this.$router.push('/seller_list');
