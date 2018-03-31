ad12cd887124652434c4c71911964f08d0f4273e
gutropolis <gutropolis@gmail.com>
2018-03-31 15:39:14 +0530

    edit charity

--- app/Seller.php
+++ app/Seller.php
@@ -8,7 +8,7 @@ class Seller extends Model
 {
  protected $table="gs_vender_organisation";
     protected $fillable = [
-        'title','description','location','pic','year_in_business','website','state','zipcode','mission_statement','vision_statement','tax_id','user_id','phone_number','business_type','post_type'
+        'title','description','location','pic','year_in_business','website','state','postal_code','area_code','mission_statement','vision_statement','tax_id','user_id','phone_number','business_type','post_type'
     ];
 
 			public function author()
