b10c264f9b70916e053fee4380c80ee768986be2
gutropolis <gutropolis@gmail.com>
2018-04-03 09:59:46 +0530

    charityfba

--- resources/assets/js/views/pages/my_ads/create_ads.vue
+++ resources/assets/js/views/pages/my_ads/create_ads.vue
@@ -12,11 +12,19 @@
                             <h3 class="dashboard__content--description--heading">Create Your Ads</h3>
                      <form class="form-horizontal form-material" id="create_ads" @submit.prevent="submit">
 					<div class="col-md-6">
-					
+					<p v-if="errors.length">
+									<b>Please correct the following error(s):</b>
+									<ul>
+									  <li v-for="error in errors">{{ error }}</li>
+									</ul>
+								  </p>
+								  
+								  <p>
+													
 					<div class="form-group">
 					   
                             <label class="login__element--box--label">Select Organisation</label>
-                            <select name="ads_type" v-model= "create_ads.ads_type" class="login__element--box--input">
+                            <select name="ads_type" v-model= "create_ads.ads_type"  class="login__element--box--input">
 							<option value="select">Select .. </option>
 							
 							<option v-for="item in items"   v-bind:value="item.title">{{item.title}}</option>
@@ -28,16 +36,16 @@
                         </div>
                         <div class="form-group">
                             <label class="login__element--box--label">Title</label>
-                            <input type="text" name="title" v-model="create_ads.title" class="login__element--box--input">
+                            <input type="text" name="title" v-model="create_ads.title" required class="login__element--box--input">
                         </div>
 						<div class="form-group">
                             <label class="login__element--box--label">What type of item are your charity seeking?</label>
-                             <textarea  type="text" name="description" v-model="create_ads.description"  rows="5"  class="login__element--box--input"></textarea>
+                             <textarea  type="text" name="description" v-model="create_ads.description" required rows="5"  class="login__element--box--input"></textarea>
                         </div>
                        
 						<div class="form-group">
                             <label class="login__element--box--label">Image</label>
-                            <input type="file"  v-on:change="onImageChange" class="login__element--box--input">
+                            <input type="file"  v-on:change="onImageChange" required class="login__element--box--input">
                         </div>
 						
 						 <div class="col-md-6" v-if="image">
@@ -58,14 +66,15 @@
 						   </div>
 						  </template>
 <script>
-
+import VeeValidate from 'vee-validate'
  import AppSidebar from '../users/sidebar.vue'
  import Vue from 'vue'
 	 export default {
 	  components: {
-            AppSidebar 
+            AppSidebar,VeeValidate
         },
 	data() {
+	
             return {
 				items:{},
 				image: '',
@@ -87,6 +96,12 @@
 					  },
        
         methods: {
+		 validateBeforeSubmit(e) {
+        this.$validator.validateAll();
+        if (!this.errors.any()) {
+            this.submitForm()
+        }
+      },
 				 fetchItems()
 			 {
 				axios.get('/api/charities').then((response) => {
