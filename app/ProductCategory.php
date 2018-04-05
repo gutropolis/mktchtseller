1c7557a67a922109e49c3c1138289ad89bff9c24
gutropolis <gutropolis@gmail.com>
2018-04-05 17:02:54 +0530

    admin add

--- app/ProductCategory.php
+++ app/ProductCategory.php
@@ -3,7 +3,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class PCategory extends model
+class ProductCategory extends model
 {
     use SoftDeletes;
     //protected $dates = ['deleted_at'];
