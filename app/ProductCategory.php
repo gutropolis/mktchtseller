fcf551e7b594c0d828e13cea759b9f1864df9567
gutropolis <gutropolis@gmail.com>
2018-04-05 13:33:40 +0530

    admin add

--- app/ProductCategory.php
+++ app/ProductCategory.php
@@ -1,26 +1,18 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
-class ProductCategory extends Model {
-
+class ProductCategory extends model
+{
     use SoftDeletes;
-
-    protected $dates = ['deleted_at'];
-
+    //protected $dates = ['deleted_at'];
     protected $table = 'gs_product_type';
-
     protected $guarded = ['id'];
-
-    public function seller()
-    {
-        return $this->hasMany(Seller::class);
-    }
-public function author()
-		{
-			return $this->belongsTo(User::class, 'user_id','user_name');
-		}
+   
+   
+	public function author()
+  {
+   return $this->belongsTo(User::class, 'user_id','user_name');
+  }
+	
 }
