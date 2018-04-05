382bcc941e80d1b88b0c5cc004c998502f83d46c
gutropolis <gutropolis@gmail.com>
2018-04-05 12:31:06 +0530

    admin add

---
+++ resources/views/admin/productcategory/edit.blade.php
@@ -0,0 +1,93 @@
+@extends('admin/layouts/default')
+
+{{-- Page title --}}
+@section('title')
+    @lang('Seller Category') :: @parent
+@stop
+
+{{-- page level styles --}}
+@section('header_styles')
+
+    <link href="{{ asset('assets/vendors/summernote/summernote.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/vendors/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/vendors/bootstrap-tagsinput/css/bootstrap-tagsinput.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/pages/blog.css') }}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/jasny-bootstrap/css/jasny-bootstrap.css') }}">
+    <!--end of page level css-->
+@stop
+
+
+{{-- Page content --}}
+@section('content')
+
+<section class="content-header">
+    <!--section starts-->
+    <h1>@lang('Edit Product Category')</h1>
+    <ol class="breadcrumb">
+        <li>
+            <a href="{{ route('admin.dashboard') }}"> <i class="livicon" data-name="home" data-size="14" data-c="#000" data-loop="true"></i>
+                @lang('general.home')
+            </a>
+        </li>
+        <li>
+            <a href="#">@lang('Category')</a>
+        </li>
+        <li class="active">@lang('Edit category ')</li>
+    </ol>
+</section>
+<!--section ends-->
+<section class="content paddingleft_right15">
+    <!--main content-->
+    <div class="row">
+        <div class="the-box no-border">
+            <!-- errors -->
+            {!! Form::model($productcategory, ['url' => URL::to('admin/productcategory/' . $productcategory->id), 'method' => 'put', 'class' => 'bf', 'files'=> true]) !!}
+            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                 <div class="row">
+                    <div class="col-sm-8">
+					
+						 
+					
+                        <div class="form-group {{ $errors->first('title', 'has-error') }}">
+                          <input type="text" name="title" placeholder="Title" class="form-control" value="{!! old('title',$productcategory->title) !!}"/> 
+						   <span class="help-block">{{ $errors->first('title', ':message') }}</span>
+                        </div>
+                          <div class="form-group ">
+                        <textarea name="description" id="description" class="form-control resize_vertical" placeholder="Description"
+                                  rows="4">{!! old('description',$productcategory->description) !!}</textarea>
+								   <span class="help-block">{{ $errors->first(
+								   'description', ':message') }}</span>
+                                            </div>
+                 
+                       
+                       
+                       
+                       
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success">@lang('publish')</button>
+                            <a href="#"
+                               class="btn btn-danger">@lang('discard')</a>
+                        </div>
+                   
+                    <!-- /.col-sm-4 --> 
+                {!! Form::close() !!}
+				
+	</div>
+	 </div>
+    </div>
+	</div>
+	 
+    <!--main content ends-->
+</section>
+@stop
+
+{{-- page level scripts --}}
+@section('footer_scripts')
+<!-- begining of page level js -->
+<!--edit blog-->
+<script src="{{ asset('assets/vendors/summernote/summernote.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/vendors/select2/js/select2.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/vendors/bootstrap-tagsinput/js/bootstrap-tagsinput.js') }}" type="text/javascript" ></script>
+<script type="text/javascript" src="{{ asset('assets/vendors/jasny-bootstrap/js/jasny-bootstrap.js') }}"></script>
+<script src="{{ asset('assets/js/pages/add_newblog.js') }}" type="text/javascript"></script>
+@stop
