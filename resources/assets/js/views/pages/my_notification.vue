8e0bdf989d92b5c2edac2c737f9b8b23850d9f62
gutropolis <gutropolis@gmail.com>
2018-04-13 20:40:32 +0530

    charity changes

--- resources/assets/js/views/pages/my_notification.vue
+++ resources/assets/js/views/pages/my_notification.vue
@@ -1,45 +1,32 @@
 <template>
-<div>
- <div id="main-wrapper">
-
-	    <section class=" my__account">
-        <div class="row">
+   <div  id="main-wrapper">
+      <section class=" my__account">
+         <div class="row">
             <app-sidebar></app-sidebar>
-	
             <div class="col-md-9 dashboard">
-                <div class="dashboard__content clearfix">
-                    <div class="dashboard__content--head">
-                        <h3 class="dashboard__content--head--heading">Notifications</h3>
-                        <a href="#" class="dashboard__content--head--link"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                            width="460.298px" height="460.297px" viewBox="0 0 460.298 460.297" style="enable-background:new 0 0 460.298 460.297;"
-                            xml:space="preserve">
-                           <g>
-                               <path d="M230.149,120.939L65.986,256.274c0,0.191-0.048,0.472-0.144,0.855c-0.094,0.38-0.144,0.656-0.144,0.852v137.041
-                                   c0,4.948,1.809,9.236,5.426,12.847c3.616,3.613,7.898,5.431,12.847,5.431h109.63V303.664h73.097v109.64h109.629
-                                   c4.948,0,9.236-1.814,12.847-5.435c3.617-3.607,5.432-7.898,5.432-12.847V257.981c0-0.76-0.104-1.334-0.288-1.707L230.149,120.939
-                                   z"/>
-                               <path d="M457.122,225.438L394.6,173.476V56.989c0-2.663-0.856-4.853-2.574-6.567c-1.704-1.712-3.894-2.568-6.563-2.568h-54.816
-                                   c-2.666,0-4.855,0.856-6.57,2.568c-1.711,1.714-2.566,3.905-2.566,6.567v55.673l-69.662-58.245
-                                   c-6.084-4.949-13.318-7.423-21.694-7.423c-8.375,0-15.608,2.474-21.698,7.423L3.172,225.438c-1.903,1.52-2.946,3.566-3.14,6.136
-                                   c-0.193,2.568,0.472,4.811,1.997,6.713l17.701,21.128c1.525,1.712,3.521,2.759,5.996,3.142c2.285,0.192,4.57-0.476,6.855-1.998
-                                   L230.149,95.817l197.57,164.741c1.526,1.328,3.521,1.991,5.996,1.991h0.858c2.471-0.376,4.463-1.43,5.996-3.138l17.703-21.125
-                                   c1.522-1.906,2.189-4.145,1.991-6.716C460.068,229.007,459.021,226.961,457.122,225.438z"/>
-                           </g>
-                       </svg>Home</a>
-                    </div>
-                    <div class="dashboard__content--outer clearfix">
+               <div class="dashboard__content clearfix">
+                  <div class="dashboard__content--head">
+                     <h3 class="dashboard__content--head--heading">Notification of Invited Charities By Sellers</h3>
+                  </div>
+                  <div class="donation_top_tab">
+                  <b-tabs>
+                     <b-tab title="Pending" active>
+                        <div>
+                           <div class="panel-body">
+                              <div class="table-responsive">
+                                  <div class="dashboard__content--outer clearfix">
                         <div class="notification__element clearfix">
                            
                             <div class="notification__element--view" v-for="item in items">
                                
-                                <div class="notification__element--listing" >
+                                <div v-for="product in item.product_detail" class="notification__element--listing" >
                                     <figure class="notification__element--listing--figure">
-                                       <a v-bind:href="item.description_url"> <img  v-bind:src="item.images" class="notification__element--listing--figure--image"></a>
+                                       <a v-bind:href="product.description_url"> <img  v-bind:src="product.images" class="notification__element--listing--figure--image"></a>
                                     </figure>
-                                    <div class="notification__element--listing--content">
-                                      <h4><a v-bind:href="item.description_url">{{item.title}}</a></h4></a>
+                                    <div v-for="product in item.product_detail" class="notification__element--listing--content">
+                                      <h4><a v-bind:href="product.description_url">{{product.title}}</a></h4></a>
                                         
-                                    <p> Donated By:- {{item.updated_by}} </p>
+                                    <p> Donated By:- {{product.updated_by}} </p>
                                     </div>
                                 </div>
                                 <div class="notification__element--view--button">
@@ -51,13 +38,80 @@
                           
                         </div>
                     </div>
-                </div>
+								 
+                              </div>
+                           </div>
+                        </div>
+                     </b-tab>
+                     <b-tab title="Accepted" >
+                        <div>
+                           <div class="panel-body">
+                              <div class="table-responsive">
+                                  <div class="dashboard__content--outer clearfix">
+                        <div class="notification__element clearfix">
+                           
+                            <div class="notification__element--view" v-for="item in accept_items">
+                               
+                                <div v-for="product in item.product_detail" class="notification__element--listing" >
+                                    <figure class="notification__element--listing--figure">
+                                       <a v-bind:href="product.description_url"> <img  v-bind:src="product.images" class="notification__element--listing--figure--image"></a>
+                                    </figure>
+                                    <div v-for="product in item.product_detail" class="notification__element--listing--content">
+                                      <h4><a v-bind:href="product.description_url">{{product.title}}</a></h4></a>
+                                        
+                                    <p> Donated By:- {{product.updated_by}} </p>
+                                    </div>
+                                </div>
+                               
+                            </div>
+                          
+                        </div>
+                    </div>
+								 
+                              </div>
+                           </div>
+                        </div>
+                     </b-tab>
+                     <b-tab title="Declined">
+                       <div>
+                           <div class="panel-body">
+                              <div class="table-responsive">
+                                  <div class="dashboard__content--outer clearfix">
+                        <div class="notification__element clearfix">
+                           
+                            <div class="notification__element--view" v-for="item in decline_items">
+                               
+                                <div v-for="product in item.product_detail" class="notification__element--listing" >
+                                    <figure class="notification__element--listing--figure">
+                                       <a v-bind:href="product.description_url"> <img  v-bind:src="product.images" class="notification__element--listing--figure--image"></a>
+                                    </figure>
+                                    <div v-for="product in item.product_detail" class="notification__element--listing--content">
+                                      <h4><a v-bind:href="product.description_url">{{product.title}}</a></h4></a>
+                                        
+                                    <p> Donated By:- {{product.updated_by}} </p>
+                                    </div>
+                                </div>
+                               
+                            </div>
+                          
+                        </div>
+                    </div>
+								 
+                              </div>
+                           </div>
+                        </div>
+                     </b-tab>
+					
+					  </b-tab>
+					 
+                  </b-tabs>
+               </div>
+               </div>
             </div>
-        </div>
-    </section>
-
-</div>
-</div>
+			
+         </div>
+      </section>
+   </div>
 </template>
 <script>
  
@@ -69,6 +123,8 @@
         },
 		data() {
             return {
+			decline_items:[],
+			accept_items:[],
 				items:[],
                 
             }
@@ -80,10 +136,12 @@
             this.fetchItems();
         },
 		 methods: {
-					fetchItems()
+		fetchItems()
 		 {
-             axios.get('/api/product_notifcation').then((response) => {
-                 this.items=response.data;
+             axios.get('/api/charity_notification').then((response) => {
+                 this.items=response.data.data1;
+				 this.accept_items=response.data.data2;
+				 this.decline_items=response.data.data3;
 					
               }) 
 			  
@@ -101,7 +159,7 @@
 			reject(id){
 				axios.post('api/reject_donation/'+id).then(response =>  {
                     toastr['success'](response.data.message);
-                    this.items=response.data;
+                    
                 })
 			 
 			
