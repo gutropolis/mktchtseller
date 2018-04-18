31b00122c04ad219123f012736d3d37642323ed0
gutropolis <gutropolis@gmail.com>
2018-04-18 15:16:50 +0530

    changes

--- resources/views/emails/seller.blade.php
+++ resources/views/emails/seller.blade.php
@@ -1,16 +1,16 @@
 <div class="container">
     <div class="row">
         <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
-<div style="border:1px solid #fff;width:100px;height:20px;text-align:center;padding:10px;background-color:red"><h1  style="color:blue;font-size:20px;">Hello</h1></div>
+<div style="border:1px solid #fff;width:1000px;height:40px;text-align:center;padding:10px;margin-left:200px;background-color:lightgray"><h1  style="color:blue;font-size:20px;">Hello!</h1></div>
 </div>
                 
                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 			   <h1 style="margin-left:200px">A New Seller Organisation Register</h1>
-                   <h2 style="font-size:15px;margin-left:200px">Email ID:- {{ $user->email }}<br />
-				User Name:- {{ $user->full_name }}<br />
-				Organisation_Name:- {{ $seller->title }}<br />
-				Location:- {{ $seller->location }}<br /> 
-				Contact:- {{$seller->area_code}}{{ $seller->phone_number }}</h2></div>
+                   <h2 style="font-size:15px;margin-left:200px">Email ID:- {{ $user->email }}</h2><br />
+				   <h2 style="font-size:15px;margin-left:200px">User Name:- {{ $user->full_name }}</h2><br />
+				   <h2 style="font-size:15px;margin-left:200px">Organisation_Name:- {{ $seller->title }}</h2><br />
+				<h2 style="font-size:15px;margin-left:200px">Location:- {{ $seller->location }}</h2><br /> 
+				<h2 style="font-size:15px;margin-left:200px">Contact:- {{$seller->area_code}}{{ $seller->phone_number }}</h2></div>
 				 <a href= "{{url('admin/seller/'.$seller->id.'/edit')}}">Click To See That Seller</a>
 </div>
 </div>
