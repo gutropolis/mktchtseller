5d7743a2d96d4b9e0d846861b1d4d60bcee9838f
gutropolis <gutropolis@gmail.com>
2018-04-18 15:16:09 +0530

    hgf

--- resources/views/emails/charity.blade.php
+++ resources/views/emails/charity.blade.php
@@ -1,18 +1,18 @@
 <div class="container">
     <div class="row">
         <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
-<div style="border:1px solid #fff;width:100px;height:20px;text-align:center;padding:10px"><h1  style="color:blue;font-size:20px;">Hello</h1></div>
+<div style="border:1px solid #fff;width:1000px;height:40px;text-align:center;padding:10px;margin-left:200px;background-color:lightgray"><h1  style="color:blue;font-size:20px;">Hello!</h1></div>
 </div>
                 
                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 			   <h1 style="margin-left:200px">A New Charity Organisation Register in CharityFba</h1>
 			   <a href= "{{url('admin/charity/'.$charity->id.'/edit')}}">Click To See That Charity</a>
 			  
-                   <h2 style="font-size:15px;margin-left:200px">Email ID:- {{ $user->email }}<br />
-				User Name:- {{ $user->full_name }}<br />
-				Organisation_Name:- {{ $charity->title }}<br />
-				Location:- {{ $charity->location }}<br /> 
-				Contact:- {{$charity->area_code}}{{ $charity->phone_number }}</h2></div>
+                   <h2 style="font-size:15px;margin-left:200px">Email ID:- {{ $user->email }}</h2><br />
+				<h2 style="font-size:15px;margin-left:200px">User Name:- {{ $user->full_name }}</h2><br />
+								<h2 style="font-size:15px;margin-left:200px">Organisation_Name:- {{ $charity->title }}</h2><br />
+								<h2 style="font-size:15px;margin-left:200px">Location:- {{ $charity->location }}</h2><br /> 
+								<h2 style="font-size:15px;margin-left:200px">Contact:- {{$charity->area_code}}{{ $charity->phone_number }}</h2></div>
 				
 </div>
 </div>
