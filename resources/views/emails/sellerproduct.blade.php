516122f0c465354c8daf8a4c49736cda788af17d
gutropolis <gutropolis@gmail.com>
2018-04-21 19:40:10 +0530

    change

--- resources/views/emails/sellerproduct.blade.php
+++ resources/views/emails/sellerproduct.blade.php
@@ -1,16 +1,22 @@
 <div class="container">
     <div class="row">
         <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
-<div style="border:1px solid #fff;width:1000px;height:40px;text-align:center;padding:10px;margin-left:200px;background-color:lightgray"><h1  style="color:blue;font-size:20px;">Hello!</h1></div>
 </div>
                 
                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			   <h1 style="margin-left:200px">A New Product Added By Seller Organisation</h1>
-                   <h2 style="font-size:15px;margin-left:200px">Organisation :- {{ $seller->title }} Add Product to Donate</h2>
-				   <h2 style="font-size:15px;margin-left:200px">Product:- {{ $sellerproduct->title }}</h2><br />
-				   <h2 style="font-size:15px;margin-left:200px">Avilable Units:- {{ $sellerproduct->units }}</h2><br />
+			    <div style="border:1px solid #fff;text-align:center;padding:10px;background-color:#e8870b;"><h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">A New Product Added By Seller Organisation</h1></div>
+                   <p style="font-size:13px;">Organisation :- {{ $seller->title }} Add Product to Donate</p>
+				   <p style="font-size:13px;">Product:- {{ $sellerproduct->title }}</p>
+				   <p style="font-size:13px;">Avilable Units:- {{ $sellerproduct->units }}</p><br /></div>
 				
-				 <a href= "{{url('admin/sellerproduct/'.$sellerproduct->id.'/edit')}}">Click To See That Seller</a>
+				<p><a href= "{{url('admin/sellerproduct/'.$sellerproduct->id.'/edit')}}"  style="font-size: 14px;
+    background: #e8870b;
+    border: 1px solid #e8870b;
+    padding: 10px;
+    display: inline-block;
+    border-radius: 5px;
+    color: #fff;
+    text-decoration: none;">Click To See That Seller</a></p>
 </div>
 </div>
 </div>
