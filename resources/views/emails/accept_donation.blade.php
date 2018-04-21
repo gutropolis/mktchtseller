e032e3a79ce859f9750ee52541500eb1c8c0502a
gutropolis <gutropolis@gmail.com>
2018-04-21 19:38:00 +0530

    change

--- resources/views/emails/accept_donation.blade.php
+++ resources/views/emails/accept_donation.blade.php
@@ -1,19 +1,27 @@
 <div class="container">
     <div class="row">
-        <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
-<div style="border:1px solid #fff;width:1000px;height:40px;padding:10px;margin-left:200px;background-color:lightgray"><h1 style="color:blue;font-size:20px; text-align:center">Charity Fba!</h1></div>
-</div>
+         <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
+
                 
                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			   <h2 style="margin-left:100px">Status of Donated Product </h2>
-			   <h4 style="margin-left:100px;color:blue;font-size:20px">Hello! {{ $reciever_user->full_name }}</h4>
+			   <div style="border:1px solid #fff;text-align:center;padding:10px;background-color:#e8870b;"><h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">A Status To Donate Product</h1></div>
+			 <p style="font-size:13px;">Hello! {{ $reciever_user->full_name }}</p><br />
 			  
-                   <h4 style="font-size:15px;margin-left:100px">
-			 {{ $charity_detail->title }} are Accepted Your Product It Will Very thankful to You.</h4><br />
-			<h4 style="font-size:15px;margin-left:100px">Here Inform Your status progress Report</h4>
-				 <h4 style="font-size:15px;margin-left:100px">Progress Status :- {{ $donation_detail->progress }} % </h4>
+                    <p style="font-size:13px;">
+			 {{ $charity_detail->title }} are Accepted Your Product It Will Very thankful to You.</p><br />
+			 <p style="font-size:13px;">Here Inform Your status progress Report</p>
+				  <p style="font-size:13px;">Progress Status :- {{ $donation_detail->progress }} % </p><br />
 				
-			<a href= "{{url('users_detail')}}"><button> Click To Communicate</button> </a>
+				
+				<p><a href= "{{url('my_notification')}}" style="font-size: 14px;
+			background: #e8870b;
+			border: 1px solid #e8870b;
+			padding: 10px;
+			display: inline-block;
+			border-radius: 5px;
+			color: #fff;
+			text-decoration: none;">Click Here To Update Status</a></p>
+			
 				
 </div>
 </div>
