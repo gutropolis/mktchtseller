f07b9a5756a798caab21486fc14f260e101efe3d
gutropolis <gutropolis@gmail.com>
2018-04-21 19:39:37 +0530

    change

--- resources/views/emails/seller.blade.php
+++ resources/views/emails/seller.blade.php
@@ -1,17 +1,23 @@
 <div class="container">
     <div class="row">
-        <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
-<div style="border:1px solid #fff;width:1000px;height:40px;text-align:center;padding:10px;margin-left:200px;background-color:lightgray"><h1  style="color:blue;font-size:20px;">Hello!</h1></div>
-</div>
+       
                 
                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			   <h1 style="margin-left:200px">A New Seller Organisation Register</h1>
-                   <h2 style="font-size:15px;margin-left:200px">Email ID:- {{ $user->email }}</h2><br />
-				   <h2 style="font-size:15px;margin-left:200px">User Name:- {{ $user->full_name }}</h2><br />
-				   <h2 style="font-size:15px;margin-left:200px">Organisation_Name:- {{ $seller->title }}</h2><br />
-				<h2 style="font-size:15px;margin-left:200px">Location:- {{ $seller->location }}</h2><br /> 
-				<h2 style="font-size:15px;margin-left:200px">Contact:- {{$seller->area_code}}{{ $seller->phone_number }}</h2></div>
-				 <a href= "{{url('admin/seller/'.$seller->id.'/edit')}}">Click To See That Seller</a>
+			   <div style="border:1px solid #fff;text-align:center;padding:10px;background-color:#e8870b;"><h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">A New Seller Organisation Register</h1></div>
+                   <p style="font-size:13px;">Email ID:- {{ $user->email }}</p>
+				   <p style="font-size:13px;">User Name:- {{ $user->full_name }}</p>
+				   <p style="font-size:13px;">Organisation_Name:- {{ $seller->title }}</p>
+				<p style="font-size:13px;">Location:- {{ $seller->location }}</p>
+				<p style="font-size:13px;">Contact:- {{$seller->area_code}}{{ $seller->phone_number }}</p></div>
+			<p><a href= "{{url('admin/seller/'.$seller->id.'/edit')}}" style="font-size: 14px;
+			background: #e8870b;
+			border: 1px solid #e8870b;
+			padding: 10px;
+			display: inline-block;
+			border-radius: 5px;
+			color: #fff;
+			text-decoration: none;">Click To See That Seller</a></p>
+				
 </div>
 </div>
 </div>
