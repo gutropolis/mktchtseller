9a7872bc723ae65c7f1d14a66cf184830b50c2be
gutropolis <gutropolis@gmail.com>
2018-04-24 20:14:26 +0530

    change

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,30 +1,23 @@
-
-
 @component('mail::layout')
-    {{-- Header --}}
-    @slot('header')
-        @component('mail::header', ['url' => config('app.url')])
-          CharityFba
-        @endcomponent
-    @endslot
-
-    {{-- Body --}}
+{{-- Header --}}
+@slot('header')
+@component('mail::header', ['url' => config('app.url')])
+CharityFba
+@endcomponent
+@endslot
+{{-- Body --}}
 # Hello
-
 We have received a new contact mail.<br />
 **Name :** {{ $data->contact_name }}<br />
 **Email :** {{ $data->contact_email }}<br />
 **Company : ** {{ $data->contact_company}}<br />
 **Subject : ** {{ $data->contact_subject}}<br />
 **Message :** {{ $data->contact_msg }}
-
-
 Thanks,
-
-    {{-- Footer --}}
-    @slot('footer')
-        @component('mail::footer')
-           &copy; 2018 All Copy right received
-        @endcomponent
-    @endslot
+{{-- Footer --}}
+@slot('footer')
+@component('mail::footer')
+&copy; 2018 All Copy right received
+@endcomponent
+@endslot
 @endcomponent
