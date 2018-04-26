398fdaf2c76075cb1b35971529392c1d183be837
gutropolis <gutropolis@gmail.com>
2018-04-26 15:07:53 +0530

    change

---
+++ resources/views/emails/activated.blade.php
@@ -0,0 +1,28 @@
+<div class="container">
+   <div class="row">
+      <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12"></div>
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+         <div style="border:1px solid #fff;text-align:center;padding:10px;background-color:#e8870b;">
+            <h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">Charity FBA!</h1>
+         </div>
+		  <p style="font-size:13px;">Hello!</p> 
+         <p style="font-size:13px;">Your account has been activated </p> 
+         <p style="font-size:13px;">Click on the link below to go to our application!</p>
+		   <p><a href= "{{url('/')}}"  style="font-size: 14px;
+         background: #e8870b;
+         border: 1px solid #e8870b;
+         padding: 10px;
+         display: inline-block;
+         border-radius: 5px;
+         color: #fff;
+         text-decoration: none;">Proceed</a></p>
+         <p style="font-size:13px;">Regards,</p>
+		  <p style="font-size:13px;">Mike and the Charity FBA Team</p>
+         <br />
+      </div>
+     
+   </div>
+</div>
+</div>
+</div>
+</div>
