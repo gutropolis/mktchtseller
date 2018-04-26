dc4968f0adf7d87b96d02f234a37b54d13086b94
gutropolis <gutropolis@gmail.com>
2018-04-26 15:08:07 +0530

    change

---
+++ resources/views/emails/activation.blade.php
@@ -0,0 +1,31 @@
+<div class="container">
+   <div class="row">
+      <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12"></div>
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+         <div style="border:1px solid #fff;text-align:center;padding:10px;background-color:#e8870b;">
+            <h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">Charity FBA!</h1>
+         </div>
+		 <p style="font-size:13px;">Hello!</p>
+         <p style="font-size:13px;">Thank you for registering an account with us.</p>
+			<p style="font-size:13px;">Click on the link below to verify your email!</p>
+		 
+		  <p><a href= "{{url('/auth'.$user->activation_token)}}"  style="font-size: 14px;
+         background: #e8870b;
+         border: 1px solid #e8870b;
+         padding: 10px;
+         display: inline-block;
+         border-radius: 5px;
+         color: #fff;
+         text-decoration: none;">Verify Now</a></p>
+		 
+         <p style="font-size:13px;">Thank you for making a difference through Charity FBA!</p>
+         <p style="font-size:13px;">Regards</p>
+		  <p style="font-size:13px;"> Mike and the Charity FBA Team</p>
+         <br />
+      </div>
+     
+   </div>
+</div>
+</div>
+</div>
+</div>
