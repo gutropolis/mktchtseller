81b5cd7cbf81a8a0f722afcdc5610d7972e5a17c
gutropolis <gutropolis@gmail.com>
2018-05-02 19:55:44 +0530

    change

---
+++ resources/views/emails/deactivaterequest.blade.php
@@ -0,0 +1,21 @@
+<div class="container">
+   <div class="row">
+      <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12"></div>
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+         <div style="border:1px solid #fff;text-align:center;padding:10px;background-color:#e8870b;">
+            <h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">Charity FBA!</h1>
+         </div>
+		 <p style="font-size:13px;">Hello!</p>
+         <p style="font-size:13px;">This mail To inform Your Charity status</p>
+		<p style="font-size:13px;"> Your Charity request Are <b>{{ $request->title }} </b> Are Deleted Now</p>
+		 <p style="font-size:13px;">Thank you for making a difference through Charity FBA!</p>
+         <p style="font-size:13px;">Regards</p>
+		  <p style="font-size:13px;"> Mike and the Charity FBA Team</p>
+         <br />
+      </div>
+     
+   </div>
+</div>
+</div>
+</div>
+</div>
