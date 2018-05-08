f8924e5fbadd9d382fce32a5011f95b46e1f41d7
gutropolis <gutropolis@gmail.com>
2018-05-08 11:27:01 +0530

    add

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,7 +13,7 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 	
-	public function AddUserActivityFeed($sender_id, $reciever_id, $subject, $post_id,$link) {
+	public function AddMessageActivityFeed($sender_id, $reciever_id, $subject, $post_id,$link) {
 					
 
 					$read_to= '0';
@@ -34,6 +34,34 @@ class Controller extends BaseController
 					$sender_user=User::where('id',$activityfeed->sender_id)->first();
 					
 					event(new MessageStatus($activityfeed,$sender_user));
+					//  event(new DonationStatusChanged($activityfeed,$sender_user));
+					 return($activityfeed);
+	   
+					$insertedId = $activityfeed->id;
+					 return intval($insertedId);
+					 
+	}
+	public function AddUserActivityFeed($sender_id, $reciever_id, $subject, $post_id,$link) {
+					
+
+					$read_to= '0';
+					$read_by_admin='0';
+					$created_at=date('Y-m-d h:i:s');
+		
+					$activityfeed = \App\User_Activity::create([
+						   'sender_id'=> $sender_id,
+						   'reciever_id'=> $reciever_id,
+						   'subject'=> $subject,
+						   'post_id'=> $post_id,
+						    'link'=> $link,
+						   'read_to'=>  intval('0'),
+						   'read_by_admin'=>  intval('0'),
+						   'created_on'=> date('Y-m-d h:i:s') 
+					]);
+					$activityfeed->save();
+					$sender_user=User::where('id',$activityfeed->sender_id)->first();
+					
+				//	event(new MessageStatus($activityfeed,$sender_user));
 					  event(new DonationStatusChanged($activityfeed,$sender_user));
 					 return($activityfeed);
 	   
