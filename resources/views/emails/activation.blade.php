3e32a16417bcba8804dd5f09e8e8c6b8dc2722d2
gutropolis <gutropolis@gmail.com>
2018-05-11 18:01:42 +0530

    add

--- resources/views/emails/activation.blade.php
+++ resources/views/emails/activation.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
    <div class="row">
       <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12"></div>
-      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">   
          <div style="border:1px solid #fff;text-align:center;padding:10px;background-color:#e8870b;">
             <h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">Charity FBA!</h1>
          </div>
