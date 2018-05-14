78acf851f48ddf4c7d8660ea1a2db6b7822b7445
gutropolis <gutropolis@gmail.com>
2018-05-14 11:44:02 +0530

    add

--- routes/api.php
+++ routes/api.php
@@ -148,7 +148,7 @@ Route::post('/message/{id}','InboxController@message');
 Route::get('/user_id','InboxController@user_id');
 Route::get('/senderinfo/{id}','InboxController@senderinfo');
 Route::get('/unread','InboxController@unread');
-Route::get('/inboxstatus/{id}','InboxController@update_inbox_status');
+Route::post('/inboxstatus/{id}','InboxController@update_inbox_status');
 
 Route::get('/unread_message_seller','InboxController@unread_message_seller');
 Route::get('/unread_message_charity','InboxController@unread_message_charity');
