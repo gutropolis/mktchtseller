382ffa5cf7236ebf38f88f3f88a876ec586353b7
gutropolis <gutropolis@gmail.com>
2018-05-16 17:53:22 +0530

    changr

---
+++ resources/views/emails/reject_request_donation.blade.php
@@ -0,0 +1,29 @@
+<div class="container">
+    <div class="row">
+               
+               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			    <div style="border:1px solid #fff;text-align:center;padding:10px;background-color:#e8870b;">
+            <h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">Status Of Request For Donation</h1>
+         </div>
+			    <p style="font-size:13px;">Hello! {{ $reciever_user->full_name }}</p>
+			    <p style="font-size:13px;">
+			<b>Your Request For {{ $product->title }}</b> are rejected.Because Of Some Issues and Some Reasons.</p>
+				Thank You
+				 <p><a href= "{{url('my_notification')}}" style="font-size: 14px;
+               background: #e8870b;
+               border: 1px solid #e8870b;
+               padding: 10px;
+               display: inline-block;
+               border-radius: 5px;
+               color: #fff;
+               text-decoration: none;">Click Here To Update Status</a></p>
+			
+			
+			
+			
+			
+</div>
+</div>
+</div>
+</div>
+</div>
