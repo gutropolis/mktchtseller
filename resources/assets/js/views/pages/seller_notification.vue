0cb9da7176b7bd67deeb1cf920d2b051a8e360d3
gutropolis <gutropolis@gmail.com>
2018-05-16 17:51:59 +0530

    changr

--- resources/assets/js/views/pages/seller_notification.vue
+++ resources/assets/js/views/pages/seller_notification.vue
@@ -9,56 +9,133 @@
             <div class="col-md-9 dashboard">
                 <div class="dashboard__content clearfix">
                     <div class="dashboard__content--head">
-                        <h3 class="dashboard__content--head--heading">Notifications</h3>
-                        <a href="#" class="dashboard__content--head--link"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                            width="460.298px" height="460.297px" viewBox="0 0 460.298 460.297" style="enable-background:new 0 0 460.298 460.297;"
-                            xml:space="preserve">
-                           <g>
-                               <path d="M230.149,120.939L65.986,256.274c0,0.191-0.048,0.472-0.144,0.855c-0.094,0.38-0.144,0.656-0.144,0.852v137.041
-                                   c0,4.948,1.809,9.236,5.426,12.847c3.616,3.613,7.898,5.431,12.847,5.431h109.63V303.664h73.097v109.64h109.629
-                                   c4.948,0,9.236-1.814,12.847-5.435c3.617-3.607,5.432-7.898,5.432-12.847V257.981c0-0.76-0.104-1.334-0.288-1.707L230.149,120.939
-                                   z"/>
-                               <path d="M457.122,225.438L394.6,173.476V56.989c0-2.663-0.856-4.853-2.574-6.567c-1.704-1.712-3.894-2.568-6.563-2.568h-54.816
-                                   c-2.666,0-4.855,0.856-6.57,2.568c-1.711,1.714-2.566,3.905-2.566,6.567v55.673l-69.662-58.245
-                                   c-6.084-4.949-13.318-7.423-21.694-7.423c-8.375,0-15.608,2.474-21.698,7.423L3.172,225.438c-1.903,1.52-2.946,3.566-3.14,6.136
-                                   c-0.193,2.568,0.472,4.811,1.997,6.713l17.701,21.128c1.525,1.712,3.521,2.759,5.996,3.142c2.285,0.192,4.57-0.476,6.855-1.998
-                                   L230.149,95.817l197.57,164.741c1.526,1.328,3.521,1.991,5.996,1.991h0.858c2.471-0.376,4.463-1.43,5.996-3.138l17.703-21.125
-                                   c1.522-1.906,2.189-4.145,1.991-6.716C460.068,229.007,459.021,226.961,457.122,225.438z"/>
-                           </g>
-                       </svg>Home</a>
+                        <h3 class="dashboard__content--head--heading">Request By Charity</h3>
+                       
                     </div>
-                    <div v-for="item in items" class="dashboard__content--outer clearfix" >
+					<div class="donation_top_tab">
+                  <b-tabs class="">
+                     <b-tab title="Pending" active>
+                        <div class="">
+                           <div class="panel-body">
+                              <div class="table-responsive">
+                    <div class="dashboard__content--outer clearfix" >
                         <div class="notification__element clearfix">
                             <div class="notification__element-bottom">
                                
                                 
                             </div>
-                            <div class="notification__element--view">
+                            <div class="notification__element--view" v-for="item in items" >
                               
                                 <div class="notification__element--listing">
-                                    <figure v-for="charity in item.sender_detail" class="notification__element--listing--figure" >
-                                       <img class="notification__element--listing--figure--image" :src="'/images/charity/'+charity.images">
+                                    <figure class="notification__element--listing--figure" >
+                                       <img  v-bind:src="item.images" class="notification__element--listing--figure--image">
                                     </figure>
 									
-                                    <div  v-for="charity in item.sender_detail"  class="notification__element--listing--content">
-                                        <router-link :to="{name: 'charity_details', params: { id: charity.id }}" ><h4>{{charity.title}}</h4></router-link>
+                                    <div   class="notification__element--listing--content"><h4>{{item.title}}</h4>
+
                                         
-                                        <p><strong>Charity Needed This Product:</strong> {{item.product}} </p><p><strong>Units Required:</strong> {{item.units}}</p>
+                                        <p><strong>Request By:- </strong> <router-link :to="{name: 'charity_details', params: { id: item.charity_id }}" > {{item.charity}}</router-link> </p><p><strong>Requested Units:- </strong> {{item.units}}</p>
                                     </div>
                                 </div>
                                 <div class="notification__element--view--button">
                                    
 									 <button class="btn notification__element--view--button--content text-White  green" v-on:click="update(item.id)" >Accept</button>
-									  <button class="btn notification__element--view--button--content  text-White red" v-on:click="reject(item.id)">Reject</button>
+									 <button class="btn notification__element--view--button--content  text-White red" v-on:click="reject(item.id)">Reject</button>
+									   <router-link :to="{name: 'charity_details', params: { id: item.charity_id }}" ><button class="btn notification__element--view--button--content text-White green">Message Seller</button></router-link>
                                 </div>
                             </div>
-                           
+                            
                             
 
                         </div>
                     </div>
                 </div>
             </div>
+			<div v-if="items.length === 0">
+								 
+									<h4  style="text-align:center;" >No Result Found Here</h4>
+									
+										</div>
+			</div>
+			 </b-tab>
+                     <b-tab title="Accepted" >
+                        <div>
+						 <div class="panel-body">
+                              <div class="table-responsive">
+                                  <div class="dashboard__content--outer clearfix">
+                        <div class="notification__element clearfix">
+                           
+                            <div class="notification__element--view" v-for="item in accept_items">
+                               
+                                <div class="notification__element--listing" >
+                                    <figure class="notification__element--listing--figure">
+                                       <a v-bind:href="item.description_url"> <img  v-bind:src="item.images" class="notification__element--listing--figure--image"></a>
+                                    </figure>
+                                    <div class="notification__element--listing--content">
+                                      <h4><a v-bind:href="item.description_url">{{item.title}}</a></h4></a>
+                                        
+                                    <p> Requested By:- {{item.charity}} </p><p>Requested Units :- {{ item.units }}</p>
+									<b>Posted On:- {{item.created_at |moment("dddd,MMM-Do-YYYY")}}</b>
+                                    </div>
+                                </div>
+                               
+                            </div>
+                          
+                        </div>
+                    </div>
+								 
+                              </div>
+                           </div>
+						    <div v-if="accept_items.length === 0">
+								 
+									<h4  style="text-align:center;" >No Result Found Here</h4>
+									
+										</div>
+                        </div>
+                     </b-tab>
+                     <b-tab title="Declined">
+                       <div>
+                           <div class="panel-body">
+                              <div class="table-responsive">
+                                  <div class="dashboard__content--outer clearfix">
+                        <div class="notification__element clearfix">
+                           
+                            <div class="notification__element--view" v-for="item in decline_items">
+                               
+                                <div  class="notification__element--listing" >
+                                    <figure class="notification__element--listing--figure">
+                                       <a v-bind:href="item.description_url"> <img  v-bind:src="item.images" class="notification__element--listing--figure--image"></a>
+                                    </figure>
+                                    <div class="notification__element--listing--content">
+                                      <h4><a v-bind:href="item.description_url">{{item.title}}</a></h4></a>
+                                        
+                                    <p> Requested By:- {{item.charity}} </p>
+									<b>Posted On:- {{item.created_at |moment("dddd,MMM-Do-YYYY")}}</b>
+                                    </div>
+                                </div>
+                               
+                            </div>
+                          
+                        </div>
+                    </div>
+								 
+                              </div>
+                           </div>
+						   <div v-if="decline_items.length === 0">
+								 
+									<h4  style="text-align:center;" >No Result Found</h4>
+									
+										</div>
+                        </div>
+                     </b-tab>
+					
+					  </b-tab>
+					 
+                  </b-tabs>
+               </div>
+               </div>
+            </div>
+			
         </div>
     </section>
 
@@ -76,7 +153,8 @@ import AppNavbar from './users/navbar.vue'
 		data() {
             return {
 				items:{},
-                 
+                 accept_items:{},
+				 decline_items:{},
             }
         },
         mounted(){
@@ -89,16 +167,21 @@ import AppNavbar from './users/navbar.vue'
 		 methods: {
 				fetchItems()
 		 {
-             axios.get('/api/request_unit').then((response) => {
-                 this.items=response.data;
-					
+             axios.get('/api/request_charity').then((response) => {
+                 this.items=response.data.data1;
+				
+				console.log(this.items.length);
+				 this.accept_items=response.data.data2;
+				 
+				 this.decline_items=response.data.data3;
               }) 
 			  
             },
 			reject(id){
 				axios.post('api/reject_request/'+id).then(response =>  {
                     toastr['success'](response.data.message);
-                   // this.items=response.data;
+                  this.fetchItems();
+				  
                 })
 			 
 			
@@ -107,7 +190,7 @@ import AppNavbar from './users/navbar.vue'
 			update(id){
 				axios.post('api/update_request/'+id).then(response =>  {
                     toastr['success'](response.data.message);
-                    
+                    this.fetchItems();
                 })
 			 
 			
