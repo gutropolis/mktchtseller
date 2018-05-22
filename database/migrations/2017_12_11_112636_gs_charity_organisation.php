33cefcaa25b2c47f8b59c20fa9cd398eb417da7b
gutropolis <gutropolis@gmail.com>
2018-05-22 16:18:08 +0530

    fkjh

--- database/migrations/2017_12_11_112636_gs_charity_organisation.php
+++ database/migrations/2017_12_11_112636_gs_charity_organisation.php
@@ -17,7 +17,7 @@ class GsCharityOrganisation extends Migration
 		{
 			$table->increments('id');
             $table->string('title');
-            $table->string('description');
+            $table->longText('description');
             $table->string('city');
             $table->string('images');
             $table->string('country');
@@ -28,8 +28,8 @@ class GsCharityOrganisation extends Migration
 			$table->string('postal_code');
             $table->string('phone_number');
             $table->string('website');
-            $table->string('vision_statement');
-            $table->string('mission_statement');
+            $table->longText('vision_statement');
+            $table->longText('mission_statement');
             $table->string('tags');
 			$table->string('post_type');
 			 $table->string('updated_by')->nullable();
