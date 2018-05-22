5024fc34e129dc31654ff9f4511c6f2e9d1119b1
gutropolis <gutropolis@gmail.com>
2018-05-22 16:13:19 +0530

    chnage

--- resources/views/emails/donate.blade.php
+++ resources/views/emails/donate.blade.php
@@ -3,16 +3,12 @@
    <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
          <div style="border:1px solid #fff;text-align:center;padding:10px;background-color:#e8870b;">
-            <h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">Invite To Pretake Charity Donation</h1>
+            <h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">Charity Offer For Donation</h1>
          </div>
          <p style="font-size:13px;">Donater Email ID:- {{ $sender_user->email }}</p>
-         <br />
          <p style="font-size:13px;">Donater Name:- {{ $sender_user->full_name }}</p>
-         <br />
-         <p style="font-size:13px;">Donate Product:- {{ $product->title }}</p>
-         <br />
-         <p style="font-size:13px;">Want to Donate Units:- {{ $sellerproduct->units }}</p>
-         <br />
+         <p style="font-size:13px;">Offer Product:- {{ $product->title }}</p>
+         <p style="font-size:13px;">Offer to Donate Units:- {{ $sellerproduct->units }}</p>
          <p><a href= "{{url('donaters')}}" style="font-size: 14px;
             background: #e8870b;
             border: 1px solid #e8870b;
