f98f4d6dac65f04002815ef6e4b6307011208f72
gutropolis <gutropolis@gmail.com>
2018-05-22 16:12:47 +0530

    chnage

--- resources/views/emails/sellerproduct.blade.php
+++ resources/views/emails/sellerproduct.blade.php
@@ -3,11 +3,12 @@
       <div  class="col-lg-8 col-md-12 col-sm-12 col-xs-12"></div>
       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
          <div style="border:1px solid #fff;text-align:center;padding:10px;background-color:#e8870b;">
-            <h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">A New Product Added By Seller Organisation</h1>
+            <h1 style="color:blue;font-size:20px;padding:0px;margin:0px;color:#fff;">A New Product Added By Seller Organisation For Donation</h1>
          </div>
-         <p style="font-size:13px;">Organisation :- {{ $seller->title }} Add Product to Donate</p>
+         <p style="font-size:13px;">Seller Company Name :- {{ $seller->title }}</p>
          <p style="font-size:13px;">Product:- {{ $sellerproduct->title }}</p>
-         <p style="font-size:13px;">Avilable Units:- {{ $sellerproduct->units }}</p>
+         <p style="font-size:13px;">Units:- {{ $sellerproduct->units }}</p>
+		 <p style="font-size:13px;">ASIN:- {{$sellerproduct->asin_url }}</p>
          <br />
       </div>
       <p><a href= "{{url('admin/sellerproduct/'.$sellerproduct->id.'/edit')}}"  style="font-size: 14px;
