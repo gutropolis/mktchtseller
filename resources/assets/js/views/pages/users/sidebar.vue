6b1037a8deacbb81bed6235ab70dad738727dc61
gutropolis <gutropolis@gmail.com>
2018-05-23 10:27:11 +0530

    add

--- resources/assets/js/views/pages/users/sidebar.vue
+++ resources/assets/js/views/pages/users/sidebar.vue
@@ -294,7 +294,7 @@
                </router-link>
             </li>
 			 
-            </div>
+            </div> 
 			<!--<li class="proflie__element--detail--list--item">
                   <router-link to="/activity_log" class="proflie__element--detail--list--item--link">
                      <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
