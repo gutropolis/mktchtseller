6c4702d53011dcdff76badb3c1b2307f17683ea9
gutropolis <gutropolis@gmail.com>
2018-05-23 17:36:10 +0530

    change

--- resources/assets/js/views/pages/manage_seller/seller_list.vue
+++ resources/assets/js/views/pages/manage_seller/seller_list.vue
@@ -25,8 +25,9 @@
                               <tr v-for="(item,index) in items.data">
                                  <td>{{index+1}}</td>
                                  <td>{{item.title}}</td>
-								 <td v-if="item.description.length>=50">
-								<p class="charity__listing--content--box--pera"><span v-text="item.description.substring(0,200)+'.......'"></span></p>
+								 
+								 <td v-if="item.description.length>=6">
+								<p class="charity__listing--content--box--pera"><span v-text="item.description.substring(0,200)+'..'"></span></p>
 								</td>
                                  
                                 <td>{{item.city}}, {{item.state}}</td>
