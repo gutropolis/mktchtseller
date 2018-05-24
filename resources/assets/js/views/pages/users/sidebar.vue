b35d8303308550a415f907895d8807d60efcbb73
gutropolis <gutropolis@gmail.com>
2018-05-24 18:30:01 +0530

    add

--- resources/assets/js/views/pages/users/sidebar.vue
+++ resources/assets/js/views/pages/users/sidebar.vue
@@ -361,6 +361,7 @@
                       Activity Log
                   </router-link>
                </li>
+			   
             <li class="proflie__element--detail--list--item">
                <a href="#" @click.prevent="logout" class="proflie__element--detail--list--item--link">
                   <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="44.816px" height="44.816px" viewBox="0 0 44.816 44.816" style="enable-background:new 0 0 44.816 44.816;" xml:space="preserve">
@@ -415,6 +416,8 @@
 			fetchnotification(){
 				axios.get('/api/unread_charity_notification').then(response=>{
 				this.unread_notification=response.data;
+				
+				console.log(this.unread_notification);
 			
    			})
 			
