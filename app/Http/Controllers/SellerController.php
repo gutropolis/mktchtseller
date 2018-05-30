81f1b2eff65d5dda793a831c46026318abc4a577
gutropolis <gutropolis@gmail.com>
2018-05-30 14:58:28 +0530

    add files

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -173,17 +173,7 @@ class SellerController extends Controller
         return response()->json(array('data1'=>$pending,'data2'=>$accept,'data3'=>$decline));
     }
     
-	 public function report_detail(Request $request,$id)
-    {
-		 $report=Donation::select('gs_donation.created_at','gs_donation.units','gs_donation.id','gs_vender_product.price','gs_vender_product.fair_value')->where('gs_donation.id','=',$id)->join('gs_vender_product','gs_vender_product.id','=','gs_donation.product_id')->get();
-	
-		  $pdf = PDF::loadView('pdf.report', compact('report'));
-			return $pdf->download('report.pdf');
-		
 
-     
-       
-    }
 
         
      public function seller_details(Request $request,$id)
@@ -504,8 +494,21 @@ class SellerController extends Controller
     {
 		
       $data=Donation::select('gs_donation.created_at','gs_donation.signature','gs_donation.id','gs_charity_organisation.mission_statement','gs_charity_organisation.vision_statement','gs_charity_organisation.postal_code','gs_charity_organisation.description','gs_charity_organisation.website','gs_charity_organisation.address','gs_vender_product.title as product','gs_donation.units','gs_charity_organisation.title as charity','gs_vender_organisation.tax_id')->join('gs_vender_product','gs_vender_product.id','=','gs_donation.product_id')->join('gs_charity_organisation','gs_charity_organisation.id','=','gs_donation.charity_id')->where('gs_donation.id',$id)->join('gs_vender_organisation','gs_vender_organisation.id','=','gs_vender_product.organisation_id')->first();
+	 
 	  return $data; 
     }
+	public function generatepdf($id)
+	{
+		
+		$data=Donation::select('gs_donation.created_at','gs_donation.signature','gs_donation.id','gs_charity_organisation.mission_statement','gs_charity_organisation.vision_statement','gs_charity_organisation.postal_code','gs_charity_organisation.description','gs_charity_organisation.website','gs_charity_organisation.address','gs_vender_product.title as product','gs_donation.units','gs_charity_organisation.title as charity','gs_vender_organisation.tax_id')->join('gs_vender_product','gs_vender_product.id','=','gs_donation.product_id')->join('gs_charity_organisation','gs_charity_organisation.id','=','gs_donation.charity_id')->where('gs_donation.id',$id)->join('gs_vender_organisation','gs_vender_organisation.id','=','gs_vender_product.organisation_id')->first();
+			
+			$pdf = PDF::loadView('pdf.products', compact('data'));
+		
+			
+			 return $pdf->save('report.pdf');
+			 
+		
+	}
 
  public function destroy_donation($id)
     {
