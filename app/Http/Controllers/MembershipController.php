c00344a614b9e26cf6a815a2505ed21851bedf0f
gutropolis <gutropolis@gmail.com>
2018-06-04 16:53:08 +0530

    add

--- app/Http/Controllers/MembershipController.php
+++ app/Http/Controllers/MembershipController.php
@@ -31,7 +31,7 @@ class MembershipController extends Controller
 {
      public function index()
     {
-			$pack=Packages::all();
+			$pack=Membership::all();
         
         
         return ($pack);
@@ -46,52 +46,19 @@ class MembershipController extends Controller
     
     public function store(Request $request)
     {
-      $user = JWTAuth::parseToken()->authenticate();
-            $create_ads = new \App\my_ads;
-            $create_ads->title=$request['title'];
-            $create_ads->description=$request['description'];            
-            $create_ads->charity_organisation=$request['ads_type'];
-           
-            $user = JWTAuth::parseToken()->authenticate();
-            $create_ads->user_id = $user->id;    
-             $create_ads->status="0";
-            $create_ads->save();
-            $create_adsId = $create_ads->id;
-			$create_ads = my_ads::where('id',$create_adsId)->first();
-			$admin_email=Settings::pluck('admin_email');
-			$admin=$admin_email[0];
-        
-         $data = array('create_ads'=>$create_ads,'user'=>$user);
-        Mail::send('emails.request', $data , function($message) use($admin)
-        {
-            $message->to($admin)->subject('Charity Request!');
-        });
-		$actvity=new Controller;
-		$actvity->AddActivityFeed($user->id,$user->id,'charity','Post Our Need For Charity Organization',$create_ads->charity_organisation,'/my_Ads');
-         return response()->json(['message' => 'Your Ads sucessfully Added']);
+      
     }
      public function activate($activation_token){
-        $request = \App\my_ads::whereActivationToken($activation_token)->first();
-		$request->status = '1';
-        $request->save();
-        return response()->json(['message' => 'Charity request has been activated!']);
+        
     }
 
      public function edit($id)
     {
-        $edit_ads = my_ads::find($id);
-         
-        $charity=Charity::all();
-            
-        return response()->json(array('data1'=>$edit_ads,'data2'=>$charity));    
-   
+        
     }
       public function destroy($id)
     {
-         $ads = my_ads::find($id);
-      $ads->delete();
-
-     return response()->json(['message' => 'Data Deleted Successfully']);
+       
     
     }
     
@@ -106,13 +73,7 @@ class MembershipController extends Controller
     public function update(Request $request,$id)
     {
         
-       $ads_update=my_ads::find($id);
-        $ads_update->title=$request->get('title');
-        $ads_update->description=$request->get('description');
-        $ads_update->charity_organisation=$request->get('charity_organisation');
-        
-        $ads_update->save();
-     return response()->json(['message' => 'Data update Successfully']);
+      
 
     
     }
