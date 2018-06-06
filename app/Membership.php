171f69f63e8a688fdaab9ed6a1a44e740a04f61e
gutropolis <gutropolis@gmail.com>
2018-06-06 19:37:23 +0530

    add file

--- app/Membership.php
+++ app/Membership.php
@@ -8,7 +8,7 @@ class Membership extends Model
 {
     protected $table="gs_membership_pack";
     protected $fillable = [
-        'package_name','amount','currency','credit_score'
+      'package_name','description','amount','currency','credit','remaning_credit'
     ];
 
     
