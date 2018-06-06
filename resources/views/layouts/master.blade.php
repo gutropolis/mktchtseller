1f9b045fd6907d2138c4b56ee0db32c15565ccb6
gutropolis <gutropolis@gmail.com>
2018-06-06 19:38:30 +0530

    add file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,6 +11,7 @@
 	 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBybmNGcXcsC2ChADSIPX0kGMUiPBqs1v0&libraries=places"></script>
 	<link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,800">
+	
     <link rel='stylesheet' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	 <link rel="stylesheet" href="/css/vue-form-wizard.min.css">
 	 <link href="/css/toastr.min.css" rel="stylesheet" type="text/css">
@@ -24,6 +25,8 @@
      <div id="root">
          <router-view></router-view>
      </div>
+	 
+	  <script src="https://checkout.stripe.com/checkout.js"></script>
 	 <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
      <script src="/js/bundle.min.js"></script>
 	 
