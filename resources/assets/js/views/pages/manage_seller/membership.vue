fe5be7d66b2c775684bc802fbe98059ab426462d
gutropolis <gutropolis@gmail.com>
2018-06-08 12:02:34 +0530

    add

--- resources/assets/js/views/pages/manage_seller/membership.vue
+++ resources/assets/js/views/pages/manage_seller/membership.vue
@@ -8,40 +8,32 @@
                   <div class="dashboard__content clearfix">
                      <div class="dashboard__content--head">
                         <h3 class="dashboard__content--head--heading">Purchase A MemberShip</h3>
-                        
                      </div>
                      <div class="chat_frame">
-                        <div class="users_messages user_active">      
-                          
+                        <div class="users_messages user_active">
                            <div class="users_messages--users_area user_active--area">
                               <div>
-                                <div  class="row membership_content">
-        
-         <div class="col-md-3" v-for="pack in packs">
-           
-         <div class="membership_content--box">
-		
-          <h6>{{pack.package_name}}</h6><span v-if="pack.id==subscription_id">(Your Plan is Active Now) </span>
-          <div class="membership_content--box--heading"><h3><span>$</span>{{pack.amount}}</h3></div>
-           <article>           
-            <p>Credit Pack Of {{pack.credit_score}} For </p>
-            <ul><li>1 theme included.</li>
-             <li>1 year of theme updates & support.</li>
-             <li>20% off future purchases.</li></ul>
-             
-           </article>
-        
-		    <button v-if="remaining_credit > 0" disabled class="btn btn-success" style="margin-bottom:10px;">Subscribe</button>
-			   <button class="btn btn-success" v-else @click="subscribe(pack.id)" style="margin-bottom:10px;">Subscribe</button>
-		
-          </div>
-           
-         </div>
-         
-      
-        
-         </div>
-         
+                                 <div  class="row membership_content">
+                                    <div class="col-md-3" v-for="pack in packs">
+                                       <div class="membership_content--box">
+                                          <h6>{{pack.package_name}}</h6>
+                                          <span v-if="pack.id==subscription_id">{{active}} </span>
+                                          <div class="membership_content--box--heading">
+                                             <h3><span>$</span>{{pack.amount}}</h3>
+                                          </div>
+                                          <article>
+                                             <p>Credit Pack Of {{pack.credit_score}} For </p>
+                                             <ul>
+                                                <li>1 theme included.</li>
+                                                <li>1 year of theme updates & support.</li>
+                                                <li>20% off future purchases.</li>
+                                             </ul>
+                                          </article>
+                                          <button v-if="active !=''" disabled class="btn btn-success" style="margin-bottom:10px;">Subscribe</button>
+                                          <button class="btn btn-success" v-else @click="subscribe(pack.id)" style="margin-bottom:10px;">Subscribe</button>
+                                       </div>
+                                    </div>
+                                 </div>
                               </div>
                            </div>
                         </div>
@@ -49,13 +41,11 @@
                   </div>
                </div>
             </div>
-   
          </section>
       </div>
    </div>
 </template>
 <script>
-
    import AppSidebar from '../users/sidebar.vue'
    import Vue from 'vue'
     Vue.use(require('vue-moment'));
@@ -65,36 +55,43 @@
           },
       data() {
               return {
-						stripeEmail: '',
-			            stripeToken: '',
-						status: false,
-						packs:[],
-						plan_id:'',
+				   active:{},
+				   stripeEmail: '',
+				   stripeToken: '',
+				   status: false,
+				   packs:[],
+				   plan_id:'',
+				   amount:'',
                      }
-   
           },
+		  
     	created: function()
-             {
-			//this.fetchplan();
-				this.fetchcredit();
-     			this.fetchItem();
-				this.stripe = StripeCheckout.configure({
+                 {
+					this.fetchactivepack();
+					this.fetchcredit();
+					this.fetchItem();
+   	
+	this.stripe = StripeCheckout.configure({
                 key: 'pk_test_aPA6GkIugbRHoAMNVzlQoHFl',
                 image: "https://stripe.com/img/documentation/checkout/marketplace.png",
                 locale: "auto",
+				amount: this.amount,
                 panelLabel: "Subscribe For",
-                email:'hunny@gmail.com',
+                email: this.getAuthUser('email'),
                 token: (token) => {
                     this.stripeToken = token.id;
                     this.stripeEmail = 'singla.nikhil4@gmail.com';
-						
-                    axios.post('/api/subscriptions', this.$data)
+					this.amount = this.amount,
+                   
+				   axios.post('/api/subscriptions', this.$data)				
                         .then(
-						
                             response => alert('Complete! Thanks for your payment!'),
-                            response => this.status = response.body.status
+                            response => this.status = response.body.status,
+								
                         )
+						//location.reload();
                 }
+				
             });
      			 	
              },
@@ -104,53 +101,65 @@
        
        methods: {
 			subscribe(id) {
-                axios.post('/api/selected_plan/'+id).then(response=>
-				{
+					axios.post('/api/selected_plan/'+id).then(response=>
+					{
 					this.selected_pack=response.data;
+					this.amount=this.selected_pack.amount;
 					this.plan_id=response.data.id;
 					 this.stripe.open({         
-                    name:this.selected_pack.package_name,
-                    zipCode: true,
-                    amount: this.selected_pack.amount,
-                });
-				});
+								name:this.selected_pack.package_name,
+								zipCode: true,
+								amount: this.selected_pack.amount * 100,
+								});
+				
+   	});
+   
                
             },
- 
-		fetchcredit()
+   
+   fetchcredit()
+   {
+   axios.get('/api/get_credit').then(response=>
+   {
+   	this.remaining_credit=response.data.remaining_credit;
+   	this.subscription_id=response.data.package_id;
+   })
+   },
+   fetchactivepack()
+   {
+		axios.get('/api/activepack').then(response=> 
 		{
-			axios.get('/api/get_credit').then(response=>
-			{
-				this.remaining_credit=response.data.remaining_credit;
-				this.subscription_id=response.data.package_id;
-			})
-	},
-		fetchItem()
-				{
-					axios.get('/api/packs').then(response=>{
-					this.packs=response.data;
-					console.log(this.activity);
-					}).catch(error=>{
-				toastr['error'](error.response.data.message);
+		this.active=response.data;	
+			
+		
+		});
+   
+   },
+   fetchItem()
+   	{
+   		axios.get('/api/packs').then(response=>{
+   		this.packs=response.data;
+   		console.log(this.activity);
+   		}).catch(error=>{
+   	toastr['error'](error.response.data.message);
              });
                  },
-         getAuthUserFullName() {
-               return this.$store.getters.getAuthUserFullName;
-           },
-   
-           getAuthUser(name)
-     {
-               return this.$store.getters.getAuthUser(name);
-           }
+        getAuthUserFullName(){
+                   return this.$store.getters.getAuthUserFullName;
+               },
+               getAuthUser(name){
+                   return this.$store.getters.getAuthUser(name);
+               },
+       
        },
-        computed: {
-           getAvatar(){
-               return '/images/user/'+this.getAuthUser('avatar');
+          computed: {
+               getAvatar(){
+                   return '/images/user/'+this.getAuthUser('avatar');
+               }
            }
    
-       }
-
               }
         
           
-      </script>
+      
+</script>
