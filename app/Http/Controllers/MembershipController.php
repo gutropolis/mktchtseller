b0693118f2887c2e6bcc7565366ae9a9c078230c
gutropolis <gutropolis@gmail.com>
2018-06-08 19:05:21 +0530

    add

--- app/Http/Controllers/MembershipController.php
+++ app/Http/Controllers/MembershipController.php
@@ -92,7 +92,8 @@ class MembershipController extends Controller
 		 $subscription->save();
 
 		}
-		
+			
+			$update=User::where('id',$user->id)->update(['trial_pack'=>'0']);
           return response()->json(['message' => 'Your Subscription Activated.']);
     }
 
